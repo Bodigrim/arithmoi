6c59e5b365806033bda3144e56c5d44b3f8f4e44
Daniel Fischer <daniel.is.fischer@googlemail.com>
2011-10-21 13:32:44 +0200

    Fix imports for ghc >= 7.3

--- Math/NumberTheory/Primes/Sieve/Eratosthenes.hs
+++ Math/NumberTheory/Primes/Sieve/Eratosthenes.hs
@@ -35,9 +35,8 @@ module Math.NumberTheory.Primes.Sieve.Eratosthenes
 #include "MachDeps.h"
 
 import Control.Monad.ST
-import Data.Array.Base (unsafeRead, unsafeWrite, unsafeAt, unsafeNewArray_)
-import Data.Array.ST
-import Data.Array.Unboxed
+import Data.Array.Base
+import Data.Array.ST        hiding (unsafeFreeze, unsafeThaw, castSTUArray)
 import Control.Monad (when)
 import Data.Bits
 import Data.Word
