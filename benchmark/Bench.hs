aa2817dd48edc5386cdedcc62f805dde08e96897
Bodigrim <andrew.lelechenko@gmail.com>
2016-06-12 16:18:15 +0300

    Write benchmark for arithmetic functions

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -1,3 +1,21 @@
 module Main where
 
-main = return ()
+import Criterion.Main
+
+import Math.NumberTheory.ArithmeticFunctions as A
+import Math.NumberTheory.Primes.Factorisation as F
+
+compareFunctions :: String -> (Integer -> Integer) -> (Integer -> Integer) -> Benchmark
+compareFunctions name old new = bgroup name
+  [ bench "old" $ nf (map old) [1..100000]
+  , bench "new" $ nf (map new) [1..100000]
+  ]
+
+main = defaultMain
+  [ compareFunctions "totient" F.totient A.totient
+  , compareFunctions "carmichael" F.carmichael A.carmichael
+  , compareFunctions "moebius" F.moebius A.moebius
+  , compareFunctions "tau" F.tau A.tau
+  , compareFunctions "sigma 1" (F.sigma 1) (A.sigma 1)
+  , compareFunctions "sigma 2" (F.sigma 2) (A.sigma 2)
+  ]
