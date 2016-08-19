6973a32384926693afba5d11aa1cce8602d21a9d
Bodigrim <andrew.lelechenko@gmail.com>
2016-08-19 18:12:01 +0300

    Standalone deriving comes to rescue

--- test-suite/Math/NumberTheory/TestUtils/Compose.hs
+++ test-suite/Math/NumberTheory/TestUtils/Compose.hs
@@ -10,54 +10,38 @@
 --
 
 {-# LANGUAGE CPP                        #-}
+{-# LANGUAGE DeriveGeneric              #-}
 {-# LANGUAGE FlexibleContexts           #-}
 {-# LANGUAGE FlexibleInstances          #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE StandaloneDeriving         #-}
+{-# LANGUAGE UndecidableInstances       #-}
 
-{-# OPTIONS_GHC -fno-warn-orphans               #-}
-#if __GLASGOW_HASKELL__ >= 800
-{-# OPTIONS_GHC -fno-warn-redundant-constraints #-}
-#endif
+{-# OPTIONS_GHC -fno-warn-orphans       #-}
 
 module Math.NumberTheory.TestUtils.Compose where
 
-#if MIN_VERSION_base(4,8,0)
-#else
-import Control.Applicative
-#endif
-import Control.Arrow
-import Data.Functor.Classes
 import Data.Functor.Compose
+#if MIN_VERSION_transformers(0,5,0)
+#else
+import GHC.Generics
+#endif
 
-import Test.Tasty.QuickCheck (Arbitrary(..))
-import Test.SmallCheck.Series (Serial(..))
+import Test.Tasty.QuickCheck (Arbitrary)
+import Test.SmallCheck.Series (Serial)
 
-instance Num (f (g a)) => Num (Compose f g a) where
-  (Compose a) + (Compose b) = Compose (a + b)
-  (Compose a) * (Compose b) = Compose (a * b)
-  abs = Compose . abs . getCompose
-  signum = Compose . signum . getCompose
-  negate = Compose . negate . getCompose
-  fromInteger = Compose . fromInteger
+deriving instance Num (f (g a))     => Num (Compose f g a)
+deriving instance Enum (f (g a))    => Enum (Compose f g a)
+deriving instance Bounded (f (g a)) => Bounded (Compose f g a)
 
-instance Enum (f (g a)) => Enum (Compose f g a) where
-  toEnum = Compose . toEnum
-  fromEnum = fromEnum . getCompose
+deriving instance (Ord (Compose f g a), Real (f (g a)))     => Real (Compose f g a)
+deriving instance (Ord (Compose f g a), Integral (f (g a))) => Integral (Compose f g a)
 
-instance Bounded (f (g a)) => Bounded (Compose f g a) where
-  minBound = Compose minBound
-  maxBound = Compose maxBound
+deriving instance Arbitrary (f (g a)) => Arbitrary (Compose f g a)
 
-instance (Functor f, Ord1 f, Ord1 g, Ord a, Real (f (g a))) => Real (Compose f g a) where
-  toRational = toRational . getCompose
-
-instance (Functor f, Ord1 f, Ord1 g, Ord a, Integral (f (g a))) => Integral (Compose f g a) where
-  quotRem (Compose a) (Compose b) = (Compose *** Compose) (quotRem a b)
-  toInteger = toInteger . getCompose
-
-instance (Monad m, Serial m (f (g a))) => Serial m (Compose f g a) where
-  series = Compose <$> series
-
-instance Arbitrary (f (g a)) => Arbitrary (Compose f g a) where
-  arbitrary = Compose <$> arbitrary
-  shrink = fmap Compose . shrink . getCompose
+#if MIN_VERSION_transformers(0,5,0)
+#else
+deriving instance Generic (Compose f g a)
+#endif
+instance (Monad m, Serial m (f (g a))) => Serial m (Compose f g a)
