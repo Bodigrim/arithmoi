2977b6cd6440ec88dd4538133eeb9ebfada79d36
Bodigrim <andrew.lelechenko@gmail.com>
2016-09-27 02:19:45 +0300

    Reimplement recurrent sequences more efficiently

--- Math/NumberTheory/Recurrencies/Bilinear.hs
+++ Math/NumberTheory/Recurrencies/Bilinear.hs
@@ -17,20 +17,22 @@
 -- > > :set +s
 -- > > binomial !! 1000 !! 1000 :: Integer
 -- > 1
--- > (0.10 secs, 153,437,432 bytes)
+-- > (0.01 secs, 1,385,512 bytes)
 -- > > binomial !! 1000 !! 1000 :: Integer
 -- > 1
--- > (0.10 secs, 153,432,856 bytes)
+-- > (0.01 secs, 1,381,616 bytes)
 --
 -- against
 --
 -- > > let binomial' = binomial :: [[Integer]]
 -- > > binomial' !! 1000 !! 1000 :: Integer
 -- > 1
--- > (0.10 secs, 152,401,808 bytes)
+-- > (0.01 secs, 1,381,696 bytes)
 -- > > binomial' !! 1000 !! 1000 :: Integer
 -- > 1
--- > (0.01 secs, 1,551,416 bytes)
+-- > (0.01 secs, 391,152 bytes)
+
+{-# LANGUAGE CPP #-}
 
 module Math.NumberTheory.Recurrencies.Bilinear
   ( binomial
@@ -43,9 +45,19 @@ module Math.NumberTheory.Recurrencies.Bilinear
 
 import Data.List
 import Data.Ratio
+import Numeric.Natural
+
+#if MIN_VERSION_base(4,8,0)
+#else
+import Data.Word
+#endif
 
 factorial :: (Num a, Enum a) => [a]
-factorial = 1 : zipWith (*) [1..] factorial
+factorial = scanl (*) 1 [1..]
+{-# SPECIALIZE factorial :: [Int]     #-}
+{-# SPECIALIZE factorial :: [Word]    #-}
+{-# SPECIALIZE factorial :: [Integer] #-}
+{-# SPECIALIZE factorial :: [Natural] #-}
 
 -- | Infinite zero-based table of binomial coefficients: @binomial !! n !! k == n! \/ k! \/ (n - k)!@.
 --
@@ -53,10 +65,23 @@ factorial = 1 : zipWith (*) [1..] factorial
 -- > [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
 --
 -- One could also consider 'Math.Combinat.Numbers.binomial' to compute stand-alone values.
-binomial :: Num a => [[a]]
-binomial = [1] : map f binomial
+binomial :: Integral a => [[a]]
+binomial = map f [0..]
   where
-    f xs = 1 : zipWith (+) xs (tail xs ++ [0])
+    f n = scanl (\x k -> x * (n - k + 1) `div` k) 1 [1..n]
+{-# SPECIALIZE binomial :: [[Int]]     #-}
+{-# SPECIALIZE binomial :: [[Word]]    #-}
+{-# SPECIALIZE binomial :: [[Integer]] #-}
+{-# SPECIALIZE binomial :: [[Natural]] #-}
+
+-- binomial_ :: Num a => [[a]]
+-- binomial_ = map f [0..]
+--   where
+--     f n = go
+-- (n k) = n! / k! / (n - k)! = n! / (k - 1)! / (n - k + 1)! * (n - k + 1) / k = (n k-1) * (n - k + 1) / k
+--
+-- {-# SPECIALIZE binomial_ :: [[Integer]] #-}
+-- {-# SPECIALIZE binomial_ :: [[Natural]] #-}
 
 -- | Infinite zero-based table of <https://en.wikipedia.org/wiki/Stirling_numbers_of_the_first_kind Stirling numbers of the first kind>.
 --
@@ -65,9 +90,13 @@ binomial = [1] : map f binomial
 --
 -- One could also consider 'Math.Combinat.Numbers.unsignedStirling1st' to compute stand-alone values.
 stirling1 :: (Num a, Enum a) => [[a]]
-stirling1 = [1] : zipWith f [0..] stirling1
+stirling1 = scanl f [1] [0..]
   where
-    f n xs = 0 : zipWith (\x1 x -> x1 + n * x) xs (tail xs ++ [0])
+    f xs n = 0 : zipIndexedListWithTail (\_ x y -> x + n * y) 1 xs 0
+{-# SPECIALIZE stirling1 :: [[Int]]     #-}
+{-# SPECIALIZE stirling1 :: [[Word]]    #-}
+{-# SPECIALIZE stirling1 :: [[Integer]] #-}
+{-# SPECIALIZE stirling1 :: [[Natural]] #-}
 
 -- | Infinite zero-based table of <https://en.wikipedia.org/wiki/Stirling_numbers_of_the_second_kind Stirling numbers of the second kind>.
 --
@@ -76,9 +105,13 @@ stirling1 = [1] : zipWith f [0..] stirling1
 --
 -- One could also consider 'Math.Combinat.Numbers.stirling2nd' to compute stand-alone values.
 stirling2 :: (Num a, Enum a) => [[a]]
-stirling2 = [1] : map f stirling2
+stirling2 = iterate f [1]
   where
-    f xs = 0 : zipWith3 (\k x1 x -> x1 + k * x) [1..] xs (tail xs ++ [0])
+    f xs = 0 : zipIndexedListWithTail (\k x y -> x + k * y) 1 xs 0
+{-# SPECIALIZE stirling2 :: [[Int]]     #-}
+{-# SPECIALIZE stirling2 :: [[Word]]    #-}
+{-# SPECIALIZE stirling2 :: [[Integer]] #-}
+{-# SPECIALIZE stirling2 :: [[Natural]] #-}
 
 -- | Infinite zero-based table of <https://en.wikipedia.org/wiki/Eulerian_number Eulerian numbers of the first kind>.
 --
@@ -86,9 +119,13 @@ stirling2 = [1] : map f stirling2
 -- > [[],[1],[1,1],[1,4,1],[1,11,11,1]]
 --
 eulerian1 :: (Num a, Enum a) => [[a]]
-eulerian1 = [] : zipWith f [1..] eulerian1
+eulerian1 = scanl f [] [1..]
   where
-    f n xs = 1 : zipWith3 (\k x1 x -> (n - k) * x1 + (k + 1) * x) [1..] xs (tail xs ++ [0])
+    f xs n = 1 : zipIndexedListWithTail (\k x y -> (n - k) * x + (k + 1) * y) 1 xs 0
+{-# SPECIALIZE eulerian1 :: [[Int]]     #-}
+{-# SPECIALIZE eulerian1 :: [[Word]]    #-}
+{-# SPECIALIZE eulerian1 :: [[Integer]] #-}
+{-# SPECIALIZE eulerian1 :: [[Natural]] #-}
 
 -- | Infinite zero-based table of <https://en.wikipedia.org/wiki/Eulerian_number#Eulerian_numbers_of_the_second_kind Eulerian numbers of the second kind>.
 --
@@ -96,9 +133,13 @@ eulerian1 = [] : zipWith f [1..] eulerian1
 -- > [[],[1],[1,2],[1,8,6],[1,22,58,24]]
 --
 eulerian2 :: (Num a, Enum a) => [[a]]
-eulerian2 = [] : zipWith f [1..] eulerian2
+eulerian2 = scanl f [] [1..]
   where
-    f n xs = 1 : zipWith3 (\k x1 x -> (2 * n - k - 1) * x1 + (k + 1) * x) [1..] xs (tail xs ++ [0])
+    f xs n = 1 : zipIndexedListWithTail (\k x y -> (2 * n - k - 1) * x + (k + 1) * y) 1 xs 0
+{-# SPECIALIZE eulerian2 :: [[Int]]     #-}
+{-# SPECIALIZE eulerian2 :: [[Word]]    #-}
+{-# SPECIALIZE eulerian2 :: [[Integer]] #-}
+{-# SPECIALIZE eulerian2 :: [[Natural]] #-}
 
 -- | Infinite zero-based sequence of <https://en.wikipedia.org/wiki/Bernoulli_number Bernoulli numbers>,
 -- computed via <https://en.wikipedia.org/wiki/Bernoulli_number#Connection_with_Stirling_numbers_of_the_second_kind connection>
@@ -112,3 +153,19 @@ bernoulli :: Integral a => [Ratio a]
 bernoulli = map f stirling2
   where
     f = sum . zipWith4 (\sgn denom fact stir -> sgn * fact * stir % denom) (cycle [1, -1]) [1..] factorial
+{-# SPECIALIZE bernoulli :: [Rational] #-}
+
+-------------------------------------------------------------------------------
+-- Utils
+
+-- zipIndexedListWithTail f n as a == zipWith3 f [n..] as (tail as ++ [a])
+-- but inlines much better and avoids checks for distinct sizes of lists.
+zipIndexedListWithTail :: Enum b => (b -> a -> a -> b) -> b -> [a] -> a -> [b]
+zipIndexedListWithTail f n as a = case as of
+  []       -> []
+  (x : xs) -> go n x xs
+  where
+    go m y ys = case ys of
+      []       -> [f m y a]
+      (z : zs) -> f m y z : go (succ m) z zs
+{-# INLINE zipIndexedListWithTail #-}
