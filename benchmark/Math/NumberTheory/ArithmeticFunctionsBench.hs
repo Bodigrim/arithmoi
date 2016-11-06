d74689a72b86486bb7edc86bafd3da4e2691367d
Bodigrim <andrew.lelechenko@gmail.com>
2016-11-06 20:41:06 +0200

    Remove deprecated arithmetic functions

--- benchmark/Math/NumberTheory/ArithmeticFunctionsBench.hs
+++ benchmark/Math/NumberTheory/ArithmeticFunctionsBench.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}
-
 module Math.NumberTheory.ArithmeticFunctionsBench
   ( benchSuite
   ) where
@@ -8,27 +6,20 @@ import Criterion.Main
 import Data.Set (Set)
 
 import Math.NumberTheory.ArithmeticFunctions as A
-import Math.NumberTheory.Primes.Factorisation as F
 
-compareFunctions :: String -> (Integer -> Integer) -> (Integer -> Integer) -> Benchmark
-compareFunctions name old new = bgroup name
-  [ bench "old" $ nf (map old) [1..100000]
-  , bench "new" $ nf (map new) [1..100000]
-  ]
+compareFunctions :: String -> (Integer -> Integer) -> Benchmark
+compareFunctions name new = bench name $ nf (map new) [1..100000]
 
-compareSetFunctions :: String -> (Integer -> Set Integer) -> (Integer -> Set Integer) -> Benchmark
-compareSetFunctions name old new = bgroup name
-  [ bench "old" $ nf (map old) [1..100000]
-  , bench "new" $ nf (map new) [1..100000]
-  ]
+compareSetFunctions :: String -> (Integer -> Set Integer) -> Benchmark
+compareSetFunctions name new = bench name $ nf (map new) [1..100000]
 
 benchSuite = bgroup "ArithmeticFunctions"
-  [ compareSetFunctions "divisors" F.divisors A.divisors
+  [ compareSetFunctions "divisors" A.divisors
   , bench "divisors/int" $ nf (map A.divisorsSmall) [1 :: Int .. 100000]
-  , compareFunctions "totient" F.totient A.totient
-  , compareFunctions "carmichael" F.carmichael A.carmichael
-  , compareFunctions "moebius" F.moebius A.moebius
-  , compareFunctions "tau" F.tau A.tau
-  , compareFunctions "sigma 1" (F.sigma 1) (A.sigma 1)
-  , compareFunctions "sigma 2" (F.sigma 2) (A.sigma 2)
+  , compareFunctions "totient" A.totient
+  , compareFunctions "carmichael" A.carmichael
+  , compareFunctions "moebius" A.moebius
+  , compareFunctions "tau" A.tau
+  , compareFunctions "sigma 1" (A.sigma 1)
+  , compareFunctions "sigma 2" (A.sigma 2)
   ]
--- benchmark/Math/NumberTheory/PowersBench.hs
+++ benchmark/Math/NumberTheory/PowersBench.hs
@@ -1,5 +1,3 @@
-{-# OPTIONS_GHC -fno-warn-warnings-deprecations #-}
-
 module Math.NumberTheory.PowersBench
   ( benchSuite
   ) where
