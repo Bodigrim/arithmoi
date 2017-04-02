86005a6aa5f4a44f0b9d50bc64592296cb47cf8f
Bodigrim <andrew.lelechenko@gmail.com>
2017-04-02 15:14:02 +0300

    Benchmark isPrime and millerRabinV

--- benchmark/Bench.hs
+++ benchmark/Bench.hs
@@ -6,10 +6,12 @@ import Criterion.Main
 
 import Math.NumberTheory.ArithmeticFunctionsBench as ArithmeticFunctions
 import Math.NumberTheory.PowersBench as Powers
+import Math.NumberTheory.PrimesBench as Primes
 import Math.NumberTheory.RecurrenciesBench as Recurrencies
 
 main = defaultMain
   [ ArithmeticFunctions.benchSuite
   , Powers.benchSuite
+  , Primes.benchSuite
   , Recurrencies.benchSuite
   ]
