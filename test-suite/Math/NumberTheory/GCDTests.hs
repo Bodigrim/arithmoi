964e4c553b956bb675c4eabe91d2ead296d2e542
Bodigrim <andrew.lelechenko@gmail.com>
2017-10-01 14:29:58 +0300

    Test splitIntoCoprimes

--- test-suite/Math/NumberTheory/GCDTests.hs
+++ test-suite/Math/NumberTheory/GCDTests.hs
@@ -8,6 +8,7 @@
 -- Tests for Math.NumberTheory.GCD
 --
 
+{-# LANGUAGE CPP                 #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
@@ -18,7 +19,14 @@ module Math.NumberTheory.GCDTests
 
 import Test.Tasty
 
+import Control.Arrow
 import Data.Bits
+import Data.List (tails)
+#if MIN_VERSION_base(4,8,0)
+#else
+import Data.Word
+#endif
+import Numeric.Natural
 
 import Math.NumberTheory.GCD
 import Math.NumberTheory.TestUtils
@@ -42,9 +50,31 @@ extendedGCDProperty (AnySign a) (AnySign b) =
 coprimeProperty :: (Integral a, Bits a) => AnySign a -> AnySign a -> Bool
 coprimeProperty (AnySign a) (AnySign b) = coprime a b == (gcd a b == 1)
 
+splitIntoCoprimesProperty1 :: [(Positive Natural, Power Word)] -> Bool
+splitIntoCoprimesProperty1 fs' = factorback fs == factorback (splitIntoCoprimes fs)
+  where
+    fs = map (getPositive *** getPower) fs'
+    factorback = product . map (uncurry (^))
+
+splitIntoCoprimesProperty2 :: [(Positive Natural, Power Word)] -> Bool
+splitIntoCoprimesProperty2 fs' = multiplicities fs <= multiplicities (splitIntoCoprimes fs)
+  where
+    fs = map (getPositive *** getPower) fs'
+    multiplicities = sum . map snd . filter ((/= 1) . fst)
+
+splitIntoCoprimesProperty3 :: [(Positive Natural, Power Word)] -> Bool
+splitIntoCoprimesProperty3 fs' = and [ coprime x y | (x : xs) <- tails fs, y <- xs ]
+  where
+    fs = map fst $ splitIntoCoprimes $ map (getPositive *** getPower) fs'
+
 testSuite :: TestTree
 testSuite = testGroup "GCD"
   [ testSameIntegralProperty "binaryGCD"   binaryGCDProperty
   , testSameIntegralProperty "extendedGCD" extendedGCDProperty
   , testSameIntegralProperty "coprime"     coprimeProperty
+  , testGroup "splitIntoCoprimes"
+    [ testSmallAndQuick "preserves product of factors"        splitIntoCoprimesProperty1
+    , testSmallAndQuick "number of factors is non-decreasing" splitIntoCoprimesProperty2
+    , testSmallAndQuick "output factors are coprime"          splitIntoCoprimesProperty3
+    ]
   ]
