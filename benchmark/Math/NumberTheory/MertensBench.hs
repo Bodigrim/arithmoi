2e331e7f776d9d3895236934035b0ca4362bdaaa
Bodigrim <andrew.lelechenko@gmail.com>
2018-01-05 23:18:24 +0000

    Implement sublinear Mertens function

---
+++ benchmark/Math/NumberTheory/MertensBench.hs
@@ -0,0 +1,21 @@
+{-# LANGUAGE CPP        #-}
+{-# LANGUAGE LambdaCase #-}
+
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Math.NumberTheory.MertensBench
+  ( benchSuite
+  ) where
+
+import Criterion.Main
+#if __GLASGOW_HASKELL__ < 709
+import Data.Word
+#endif
+
+import Math.NumberTheory.ArithmeticFunctions.Mertens
+
+mertensBench :: Word -> Benchmark
+mertensBench n = bench (show n) (nf mertens n)
+
+benchSuite :: Benchmark
+benchSuite = bgroup "Mertens" $ map mertensBench $ take 4 $ iterate (* 10) 10000000
