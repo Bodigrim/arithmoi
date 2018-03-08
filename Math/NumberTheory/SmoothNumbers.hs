45a95204af056a04715e56869f2d3acbb5a49799
Frederick Schneider <frederick.schneider2011@gmail.com>
2018-03-08 13:09:15 -0500

    For clarity, rename fromSmoothUB to fromSmoothUpperBound and validU to isValid.  Additional comments added.

--- Math/NumberTheory/SmoothNumbers.hs
+++ Math/NumberTheory/SmoothNumbers.hs
@@ -15,7 +15,7 @@ module Math.NumberTheory.SmoothNumbers
   , smoothOverInRangeBF
   , fromSet
   , fromList
-  , fromSmoothUB
+  , fromSmoothUpperBound
   ) where
 
 import Data.List (sort, nub)
@@ -24,18 +24,22 @@ import Math.NumberTheory.Primes.Sieve (primes)
 
 newtype SmoothBasis a = SmoothBasis [a] deriving Show
 
+-- Returns a SmoothBasis from a Set if it is valid.
 fromSet :: Integral a => S.Set a -> Maybe (SmoothBasis a)
-fromSet s = if validU l then Just (SmoothBasis l) else Nothing where l = S.elems s
+fromSet s = if isValid l then Just (SmoothBasis l) else Nothing where l = S.elems s
 
+-- Returns a SmoothBasis from a list if it is valid.
 fromList :: Integral a => [a] -> Maybe (SmoothBasis a)
-fromList l = if validU l' then Just (SmoothBasis l') else Nothing where l' = nub $ sort l
+fromList l = if isValid l' then Just (SmoothBasis l') else Nothing where l' = nub $ sort l
 
-fromSmoothUB :: Integral a => a -> Maybe (SmoothBasis a)
-fromSmoothUB n = if (n < 2)
-                 then Nothing
-                 else Just $ SmoothBasis (map fromInteger $ takeWhile (<= nI) primes)
-                 where nI = toInteger n
+-- | Return the primes <= the upper bound param.
+fromSmoothUpperBound :: Integral a => a -> Maybe (SmoothBasis a)
+fromSmoothUpperBound n = if (n < 2)
+                         then Nothing
+                         else Just $ SmoothBasis $ map fromInteger $ takeWhile (<= nI) primes
+                         where nI = toInteger n
 
+-- | Return the list from a SmoothBasis.
 fromSmoothBasis :: Integral a => SmoothBasis a -> [a]
 fromSmoothBasis (SmoothBasis l) = l
 
@@ -65,12 +69,11 @@ smoothOverInRangeBF prs lo hi = filter (mf prs') [lo..hi]
                                                        else mf ps n
                                       prs'           = fromSmoothBasis prs
 
--- | validU assumes that the list is sorted and unique and then checks if the list is further suitable for smoothU (see below). 
---   U is for unsafe.  It should not be called directly.
-validU :: (Integral a) => [a] -> Bool
-validU pl = if (length pl == 0) then False else v' pl
-            where v' []        = True
-                  v' (x:xs)    = if (x < 2 || (not $ rpl x xs)) then False else v' xs
-                  rpl _ []     = True  -- rpl means relatively prime to the rest of the list
-                  rpl n (x:xs) = if (gcd n x > 1) then False else rpl n xs
+-- isValid assumes that the list is sorted and unique and then checks if the list is suitable to be a SmoothBasis. 
+isValid :: (Integral a) => [a] -> Bool
+isValid pl = if (length pl == 0) then False else v' pl
+             where v' []        = True
+                   v' (x:xs)    = if (x < 2 || (not $ rpl x xs)) then False else v' xs
+                   rpl _ []     = True  -- rpl means relatively prime to the rest of the list
+                   rpl n (x:xs) = if (gcd n x > 1) then False else rpl n xs
 
