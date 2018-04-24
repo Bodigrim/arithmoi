88ceffa6b342eb02a69cad339ffe12fc198492d1
Bodigrim <andrew.lelechenko@gmail.com>
2018-04-24 22:34:30 +0100

    Benchmark smooth numbers

---
+++ benchmark/Math/NumberTheory/SmoothNumbersBench.hs
@@ -0,0 +1,22 @@
+{-# OPTIONS_GHC -fno-warn-type-defaults #-}
+
+module Math.NumberTheory.SmoothNumbersBench
+  ( benchSuite
+  ) where
+
+import Data.List (genericTake)
+import Data.Maybe
+import Gauge.Main
+import Numeric.Natural
+
+import Math.NumberTheory.SmoothNumbers
+
+doBench :: Integral a => a -> a
+doBench lim = sum $ genericTake lim $ smoothOver $ fromJust $ fromSmoothUpperBound lim
+
+benchSuite :: Benchmark
+benchSuite = bgroup "SmoothNumbers"
+  [ bench "100"      $ nf doBench    (100 :: Int)
+  , bench "1000"     $ nf doBench   (1000 :: Int)
+  , bench "10000"    $ nf doBench  (10000 :: Int)
+  ]
