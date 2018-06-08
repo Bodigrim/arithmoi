88f5e9db98a9bbbc5913a0ae018dd97184f1c0b9
Vit Guzeev <vitaliy@getshoptv.com>
2018-06-08 19:44:30 +0200

    Add interface for quadratic residues square root computation

--- test-suite/Math/NumberTheory/Moduli/SqrtTests.hs
+++ test-suite/Math/NumberTheory/Moduli/SqrtTests.hs
@@ -33,6 +33,8 @@ unwrapP (PrimeWrapper p) = unPrime p
 unwrapPP :: (PrimeWrapper Integer, Power Int) -> (Integer, Int)
 unwrapPP (p, Power e) = (unwrapP p, e)
 
+-- FIXME: update names. Maybe not - they describe specific behavior inside interfaces.
+
 -- | Check that 'sqrtMod' is defined iff a quadratic residue exists.
 --   Also check that the result is a solution of input modular equation.
 sqrtModPProperty :: AnySign Integer -> PrimeWrapper Integer -> Bool
@@ -46,25 +48,25 @@ sqrtModPListProperty (AnySign n) (unwrapP -> p) = all (\rt -> rt ^ 2 `mod` p == 
 sqrtModP'Property :: Positive Integer -> PrimeWrapper Integer -> Bool
 sqrtModP'Property (Positive n) (unwrapP -> p) = (p /= 2 && jacobi n p /= One) || rt ^ 2 `mod` p == n `mod` p
   where
-    rt = sqrtModP' n p
+    rt = sqrtModSharp n p
 
 tonelliShanksProperty1 :: Positive Integer -> PrimeWrapper Integer -> Bool
 tonelliShanksProperty1 (Positive n) (unwrapP -> p) = p `mod` 4 /= 1 || jacobi n p /= One || rt ^ 2 `mod` p == n `mod` p
   where
-    rt = tonelliShanks n p
+    rt = sqrtModSharp n p
 
 tonelliShanksProperty2 :: PrimeWrapper Integer -> Bool
 tonelliShanksProperty2 (unwrapP -> p) = p `mod` 4 /= 1 || rt ^ 2 `mod` p == n `mod` p
   where
     n  = head $ filter (\s -> jacobi s p == One) [2..p-1]
-    rt = tonelliShanks n p
+    rt = sqrtModSharp n p
 
 tonelliShanksSpecialCases :: Assertion
 tonelliShanksSpecialCases =
   assertEqual "OEIS A002224" [6, 32, 219, 439, 1526, 2987, 22193, 11740, 13854, 91168, 326277, 232059, 3230839, 4379725, 11754394, 32020334, 151024619, 345641931, 373671108, 1857111865, 8110112775, 4184367042] rts
   where
     ps = [17, 73, 241, 1009, 2689, 8089, 33049, 53881, 87481, 483289, 515761, 1083289, 3818929, 9257329, 22000801, 48473881, 175244281, 427733329, 898716289, 8114538721, 9176747449, 23616331489]
-    rts = map (\p -> tonelliShanks 2 p) ps
+    rts = map (\p -> sqrtModSharp 2 p) ps
 
 sqrtModPPProperty :: AnySign Integer -> (PrimeWrapper Integer, Power Int) -> Bool
 sqrtModPPProperty (AnySign n) (unwrapP -> p, Power e) = gcd n p > 1 || case sqrtModMaybe n (p ^ e) of
