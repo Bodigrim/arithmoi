d1321c1429299588f8b2006dc797979ae870305b
Vit Guzeev <vitaliy@getshoptv.com>
2018-06-09 17:34:25 +0200

    Remove partial function from Sqrt tests

--- test-suite/Math/NumberTheory/Moduli/SqrtTests.hs
+++ test-suite/Math/NumberTheory/Moduli/SqrtTests.hs
@@ -27,10 +27,6 @@ import Math.NumberTheory.Moduli hiding (invertMod, unPrime)
 import Math.NumberTheory.UniqueFactorisation (unPrime, isPrime)
 import Math.NumberTheory.TestUtils
 
-fromJustCustom :: Maybe a -> a
-fromJustCustom Nothing = error "Not a prime"
-fromJustCustom (Just a) = a
-
 unwrapP :: PrimeWrapper Integer -> Integer
 unwrapP (PrimeWrapper p) = unPrime p
 
@@ -42,39 +38,39 @@ unwrapPP (p, Power e) = (unwrapP p, e)
 -- | Check that 'sqrtMod' is defined iff a quadratic residue exists.
 --   Also check that the result is a solution of input modular equation.
 sqrtModPProperty :: AnySign Integer -> PrimeWrapper Integer -> Bool
-sqrtModPProperty (AnySign n) (unwrapP -> p) = case sqrtModMaybe n (fromJustCustom . toFieldCharacteristic $ p) of
+sqrtModPProperty (AnySign n) (unwrapP -> p) = case sqrtModMaybe n (FieldCharacteristic (primeF p) 1) of
   Nothing -> jacobi n p == MinusOne
   Just rt -> (p == 2 || jacobi n p /= MinusOne) && rt ^ 2 `mod` p == n `mod` p
 
 sqrtModPListProperty :: AnySign Integer -> PrimeWrapper Integer -> Bool
-sqrtModPListProperty (AnySign n) (unwrapP -> p) = all (\rt -> rt ^ 2 `mod` p == n `mod` p) (sqrtModList n (fromJustCustom . toFieldCharacteristic $ p))
+sqrtModPListProperty (AnySign n) (unwrapP -> p) = all (\rt -> rt ^ 2 `mod` p == n `mod` p) (sqrtModList n (FieldCharacteristic (primeF p) 1))
 
 sqrtModP'Property :: Positive Integer -> PrimeWrapper Integer -> Bool
 sqrtModP'Property (Positive n) (unwrapP -> p) = (p /= 2 && jacobi n p /= One) || rt ^ 2 `mod` p == n `mod` p
   where
-    rt = sqrtModExact n (fromJustCustom . toFieldCharacteristic $ p)
+    rt = sqrtModExact n (FieldCharacteristic (primeF p) 1)
 
 tonelliShanksProperty1 :: Positive Integer -> PrimeWrapper Integer -> Bool
 tonelliShanksProperty1 (Positive n) (unwrapP -> p) = p `mod` 4 /= 1 || jacobi n p /= One || rt ^ 2 `mod` p == n `mod` p
   where
-    rt = sqrtModExact n (fromJustCustom . toFieldCharacteristic $ p)
+    rt = sqrtModExact n (FieldCharacteristic (primeF p) 1)
 
 tonelliShanksProperty2 :: PrimeWrapper Integer -> Bool
 tonelliShanksProperty2 (unwrapP -> p) = p `mod` 4 /= 1 || rt ^ 2 `mod` p == n `mod` p
   where
     n  = head $ filter (\s -> jacobi s p == One) [2..p-1]
-    rt = sqrtModExact n (fromJustCustom . toFieldCharacteristic $ p)
+    rt = sqrtModExact n (FieldCharacteristic (primeF p) 1)
 
 tonelliShanksSpecialCases :: Assertion
 tonelliShanksSpecialCases =
   assertEqual "OEIS A002224" [6, 32, 219, 439, 1526, 2987, 22193, 11740, 13854, 91168, 326277, 232059, 3230839, 4379725, 11754394, 32020334, 151024619, 345641931, 373671108, 1857111865, 8110112775, 4184367042] rts
   where
     ps = [17, 73, 241, 1009, 2689, 8089, 33049, 53881, 87481, 483289, 515761, 1083289, 3818929, 9257329, 22000801, 48473881, 175244281, 427733329, 898716289, 8114538721, 9176747449, 23616331489]
-    rts = map (\p -> sqrtModExact 2 (fromJustCustom . toFieldCharacteristic $ p)) ps
+    rts = map (\p -> sqrtModExact 2 (FieldCharacteristic (primeF p) 1)) ps
 
 sqrtModPPProperty :: AnySign Integer -> (PrimeWrapper Integer, Power Int) -> Bool
 sqrtModPPProperty (AnySign n) (unwrapP -> p, Power e) =
-  gcd n p > 1 || case sqrtModMaybe n (fromJustCustom . toFieldCharacteristic $ p ^ e) of
+  gcd n p > 1 || case sqrtModMaybe n (FieldCharacteristic (primeF p) e) of
     Nothing -> True
     Just rt -> rt ^ 2 `mod` (p ^ e) == n `mod` (p ^ e)
 
@@ -83,15 +79,15 @@ sqrtModPPBase2Property n e = sqrtModPPProperty n (PrimeWrapper $ fromJust $ isPr
 
 sqrtModPPSpecialCase1 :: Assertion
 sqrtModPPSpecialCase1 =
-  assertEqual "sqrtModPP 16 2 2 = 4" (Just 0) (sqrtModMaybe 16 (fromJustCustom . toFieldCharacteristic $ 2 ^ 2))
+  assertEqual "sqrtModPP 16 2 2 = 4" (Just 0) (sqrtModMaybe 16 (FieldCharacteristic (primeF 2) 2))
 
 sqrtModPPSpecialCase2 :: Assertion
 sqrtModPPSpecialCase2 =
-  assertEqual "sqrtModPP 16 3 2 = 4" (Just 4) (sqrtModMaybe 16 (fromJustCustom . toFieldCharacteristic $ 3 ^ 2))
+  assertEqual "sqrtModPP 16 3 2 = 4" (Just 4) (sqrtModMaybe 16 (FieldCharacteristic (primeF 3) 2))
 
 sqrtModPPListProperty :: AnySign Integer -> (PrimeWrapper Integer, Power Int) -> Bool
 sqrtModPPListProperty (AnySign n) (unwrapP -> p, Power e) = gcd n p > 1
-  || all (\rt -> rt ^ 2 `mod` (p ^ e) == n `mod` (p ^ e)) (sqrtModList n (fromJustCustom . toFieldCharacteristic $ p ^ e))
+  || all (\rt -> rt ^ 2 `mod` (p ^ e) == n `mod` (p ^ e)) (sqrtModList n (FieldCharacteristic (primeF p) e))
 
 sqrtModFProperty :: AnySign Integer -> [(PrimeWrapper Integer, Power Int)] -> Bool
 sqrtModFProperty (AnySign n) (map unwrapPP -> pes) = case sqrtModF n pes of
