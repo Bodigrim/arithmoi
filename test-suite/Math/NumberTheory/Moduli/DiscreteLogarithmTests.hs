91c9f39bed89cd494840a5634dfcc3388205b421
Nathan van Doorn <nvd1234@gmail.com>
2018-06-10 13:08:31 +0100

    Add another test for discrete logarithm
    This test currently fails and needs to be resolved.

--- test-suite/Math/NumberTheory/Moduli/DiscreteLogarithmTests.hs
+++ test-suite/Math/NumberTheory/Moduli/DiscreteLogarithmTests.hs
@@ -19,7 +19,14 @@ discreteLogarithmProperty (Positive m) a b = fromMaybe True $ case modulo a m of
       Just $ a' ^% e == b'
     InfMod  {} -> error "Impossible"
 
+-- | Check that 'discreteLogarith' finds a solution when one exists
+discreteLogarithm2Property :: Positive Natural -> Integer -> NonNegative Integer -> Bool
+discreteLogarithm2Property (Positive m) a (NonNegative e) = isJust $ case a `modulo` m of
+  SomeMod a' -> let b = a' ^% e in discreteLogarithm a' b
+  InfMod {} -> error "Impossible"
+
 testSuite :: TestTree
 testSuite = testGroup "Discrete logarithm"
   [ testSmallAndQuick "discreteLogarithm" discreteLogarithmProperty
+  , testSmallAndQuick "discreteLogarithm2" discreteLogarithm2Property
   ]
