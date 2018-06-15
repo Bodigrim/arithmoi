66d1ee9cfd96a0b3923cb2eb6e9f9ecddd0a3507
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-06-15 19:30:28 +0100

    Refactored benching and added point ramanujan

--- benchmark/Math/NumberTheory/ArithmeticFunctionsBench.hs
+++ benchmark/Math/NumberTheory/ArithmeticFunctionsBench.hs
@@ -7,21 +7,23 @@ import Data.Set (Set)
 
 import Math.NumberTheory.ArithmeticFunctions as A
 
-compareFunctions :: String -> (Integer -> Integer) -> Benchmark
-compareFunctions name new = bench name $ nf (map new) [1..100000]
+compareFunctions :: String -> (Integer -> Integer) -> [Integer] -> Benchmark
+compareFunctions name new range = bench name $ nf (map new) range
 
 compareSetFunctions :: String -> (Integer -> Set Integer) -> Benchmark
 compareSetFunctions name new = bench name $ nf (map new) [1..100000]
 
 benchSuite :: Benchmark
 benchSuite = bgroup "ArithmeticFunctions"
-  [ compareSetFunctions "divisors" A.divisors
-  , bench "divisors/int" $ nf (map A.divisorsSmall) [1 :: Int .. 100000]
-  , compareFunctions "totient" A.totient
-  , compareFunctions "carmichael" A.carmichael
-  , compareFunctions "moebius" (A.runMoebius . A.moebius)
-  , compareFunctions "tau" A.tau
-  , compareFunctions "sigma 1" (A.sigma 1)
-  , compareFunctions "sigma 2" (A.sigma 2)
-  , bench "ramanujan" $ nf (map (ramanujan :: Integer -> Integer)) [1 .. 2000]
+  [ compareSetFunctions "divisors" A.divisors 
+  , bench "divisors/int" $ nf (map A.divisorsSmall)              [1 :: Int .. 100000]
+  , compareFunctions "totient" A.totient                         [1..100000]
+  , compareFunctions "carmichael" A.carmichael                   [1..100000]
+  , compareFunctions "moebius" (A.runMoebius . A.moebius)        [1..100000]
+  , compareFunctions "tau" A.tau                                 [1..100000]
+  , compareFunctions "sigma 1" (A.sigma 1)                       [1..100000]
+  , compareFunctions "sigma 2" (A.sigma 2)                       [1..100000]
+  , compareFunctions "ramanujan range" ramanujan                 [1..2000]
+  , compareFunctions "ramanujan large prime" ramanujan           [100003]
+  , compareFunctions "ramanujan prime power" ramanujan           [2^100]
   ]
