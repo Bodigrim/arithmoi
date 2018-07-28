ed802a8d47670f470b601e78c278188331089ef3
Alexandre Baldé <alexandrer_b@outlook.com>
2018-07-28 03:50:53 +0000

    [#107] Use a 'Map' to memoize previous values of 'partition'

--- Math/NumberTheory/Recurrencies.hs
+++ Math/NumberTheory/Recurrencies.hs
@@ -1,5 +1,6 @@
 -- Values of <https://en.wikipedia.org/wiki/Partition_(number_theory)#Partition_function partition function>.
 
+{-# LANGUAGE BangPatterns #-}
 {-# LANGUAGE RankNTypes #-}
 
 module Math.NumberTheory.Recurrencies
@@ -8,6 +9,7 @@ module Math.NumberTheory.Recurrencies
   , pents
   ) where
 
+import qualified Data.Map as M
 import Numeric.Natural (Natural)
 
 -- | Produce an infinite list of indices used by generalized pentagonal
@@ -61,28 +63,32 @@ pentagonalSigns = helper True
 
 -- @partition !! n@ calculates the @n@-th partition number:
 -- @p(n) = p(n-1) + p(n-2) - p(n-5) - p(n-7) + p(11) + ...@, where @p(0) = 1@
--- and @p(k) = 0@ for a negative integer @k@.
+-- and @p(k) = 0@ for a negative integer @k@. Uses a @Map@ from the
+-- @containers@ package to memoize previous results.
 -- 
--- Example: calculating @partition !! 10@.
+-- Example: calculating @partition !! 10@, assuming the memoization map is
+-- filled and called @dict :: Integral a => Map a a@.
 -- 
 -- * @tail [0, 1, 2, 5, 7, 12 ,15, 22, 26, 35, ..] == [1, 2, 5, 7, 12 ,15, 22, 26, 35, 40, ..]@.
 -- * @takeWhile (\m -> 10 - m >= 0) == [1, 2, 5, 7]@.
--- * @map (\m -> partition !! fromIntegral (10 - m)) [1, 2, 5, 7] ==
--- [partition !! 9, partition !! 8, partition !! 5, partition !! 3] == [30, 22, 7, 3]@
+-- * @map (\m -> dict ! fromIntegral (10 - m)) [1, 2, 5, 7] == [dict ! 9, dict ! 8, dict ! 5, dict ! 3] == [30, 22, 7, 3]@
 -- * @pentagonalSigns [30, 22, 7, 3] == [30, 22, 7, 3] == [30, 22, -7, -3]@
 -- * @sum [30, 22, -7, -3] == 42@
 --
 -- Note: @tail@ is applied to @pents@ because otherwise the calculation of
 -- @p(n)@ would involve a duplicated @p(n-1)@ term (see the above example).
 partition :: forall a . Integral a => [a]
-partition = 1 : map parts [1..]
+partition = 1 : go (M.insert 0 1 mempty) 1
   where
-    parts :: Integral a => a -> a
-    parts n = (sum .
-               pentagonalSigns .
-               map (\m -> partition !! fromIntegral (n - m)) .
-               takeWhile (\m -> n - m >= 0) .
-               tail) pents
+    go :: Integral a => M.Map a a -> a -> [a]
+    go dict !n =
+        let n' = (sum .
+                 pentagonalSigns .
+                 map (\m -> dict M.! fromIntegral (n - m)) .
+                 takeWhile (\m -> n - m >= 0) .
+                 tail) pents
+            dict' = M.insert n n' dict
+        in n' : go dict' (n + 1)
 {-# SPECIALIZE partition :: [Int]     #-}
 {-# SPECIALIZE partition :: [Word]    #-}
 {-# SPECIALIZE partition :: [Integer] #-}
