d4b6ff25c83ea2a6ec800f7d3676f3d8ed18ce58
Bodigrim <andrew.lelechenko@gmail.com>
2018-07-30 00:05:01 +0100

    Benchmark tau on Gaussian integers

--- benchmark/Math/NumberTheory/GaussianIntegersBench.hs
+++ benchmark/Math/NumberTheory/GaussianIntegersBench.hs
@@ -8,12 +8,19 @@ module Math.NumberTheory.GaussianIntegersBench
 import Control.DeepSeq
 import Gauge.Main
 
+import Math.NumberTheory.ArithmeticFunctions (tau)
 import Math.NumberTheory.GaussianIntegers
 
 instance NFData GaussianInteger
 
-doBench :: Integer -> Benchmark
-doBench n = bench ("findPrime'/" ++ show n) $ nf findPrime' n
+benchFindPrime :: Integer -> Benchmark
+benchFindPrime n = bench (show n) $ nf findPrime' n
+
+benchTau :: Integer -> Benchmark
+benchTau n = bench (show n) $ nf (\m -> sum [tau (x :+ y) | x <- [1..m], y <- [0..m]] :: Word) n
 
 benchSuite :: Benchmark
-benchSuite = bgroup "Gaussian" $ map doBench [1000033, 10000121, 100000037, 1000000009, 10000000033, 100000000057, 1000000000061, 10000000000037]
+benchSuite = bgroup "Gaussian"
+  [ bgroup "findPrime" $ map benchFindPrime [1000033, 10000121, 100000037, 1000000009, 10000000033, 100000000057, 1000000000061, 10000000000037]
+  , bgroup "tau" $ map benchTau [10, 20, 40, 80]
+  ]
