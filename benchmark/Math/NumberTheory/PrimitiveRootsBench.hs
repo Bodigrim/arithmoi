b85114bc665c491ed57130619d140e1854d5fe63
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-08-17 06:15:04 +0100

    Added benchmarks for primitive root

---
+++ benchmark/Math/NumberTheory/PrimitiveRootsBench.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE BangPatterns #-}
+
+module Math.NumberTheory.PrimitiveRootsBench
+  ( benchSuite
+  ) where
+
+import Gauge.Main
+import Data.Maybe
+
+import Math.NumberTheory.Moduli.PrimitiveRoot
+import Math.NumberTheory.UniqueFactorisation
+
+primRootWrap :: Integer -> Word -> Integer -> Bool
+primRootWrap p k g = isPrimitiveRoot' (CGOddPrimePower p' k) g
+  where p' = fromJust $ isPrime p
+
+primRootWrap2 :: Integer -> Word -> Integer -> Bool
+primRootWrap2 p k g = isPrimitiveRoot' (CGDoubleOddPrimePower p' k) g
+  where p' = fromJust $ isPrime p
+
+-- contrived function which forces the cyclic group to be fully calculated 
+forceCyclic :: Maybe (CyclicGroup a) -> Int
+forceCyclic Nothing    = 0
+forceCyclic (Just CG2) = -2
+forceCyclic (Just CG4) = -1
+forceCyclic (Just (CGOddPrimePower !_p !_k)) = 1
+forceCyclic (Just (CGDoubleOddPrimePower !_p !_k)) = 2
+
+cyclicWrap :: Integer -> Int
+cyclicWrap = forceCyclic . cyclicGroupFromModulo
+
+benchSuite :: Benchmark
+benchSuite = bgroup "PrimRoot"
+  [ bgroup "groupFromModulo" 
+    [ bench "3^20000"             $ nf cyclicWrap $! (3^20000)             -- prime to large power
+    , bench "10000000000000061"   $ nf cyclicWrap $! (10^16 + 61)          -- large prime
+    , bench "2*3^20000"           $ nf cyclicWrap $! (2*3^20000)           -- twice prime to large power
+    , bench "10000000000000046"   $ nf cyclicWrap $! (10^16 + 46)          -- twice large prime
+    , bench "224403121196654400"  $ nf cyclicWrap $! (224403121196654400)  -- highly composite
+    ]
+  , bgroup "check prim roots"
+    [ bench "3^20000"             $ nf (primRootWrap  3              20000) 2 -- prime to large power
+    , bench "10000000000000061"   $ nf (primRootWrap  (10^16 + 61)   1)     3 -- large prime
+    , bench "10000000000000061^2" $ nf (primRootWrap  (10^16 + 61)   2)     3 -- large prime squared
+    , bench "2*3^20000"           $ nf (primRootWrap2 3              20000) 5 -- twice prime to large power
+    , bench "10000000000000046"   $ nf (primRootWrap2 (5*10^15 + 23) 1)     5 -- twice large prime
+    ]
+  ]
