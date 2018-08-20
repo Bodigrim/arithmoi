8dbd51d84058decf6bf4796c48f74cc783da1a20
Bodigrim <andrew.lelechenko@gmail.com>
2018-08-21 00:13:44 +0100

    Remove redundant pragmas from PrimitiveRootsBench

--- benchmark/Math/NumberTheory/PrimitiveRootsBench.hs
+++ benchmark/Math/NumberTheory/PrimitiveRootsBench.hs
@@ -1,6 +1,3 @@
-{-# LANGUAGE BangPatterns      #-}
-{-# LANGUAGE FlexibleInstances #-}
-
 {-# OPTIONS_GHC -fno-warn-type-defaults #-}
 
 module Math.NumberTheory.PrimitiveRootsBench
@@ -27,11 +24,11 @@ cyclicWrap = cyclicGroupFromModulo
 benchSuite :: Benchmark
 benchSuite = bgroup "PrimRoot"
   [ bgroup "groupFromModulo"
-    [ bench "3^20000"             $ nf cyclicWrap $! (3^20000)             -- prime to large power
-    , bench "10000000000000061"   $ nf cyclicWrap $! (10^16 + 61)          -- large prime
-    , bench "2*3^20000"           $ nf cyclicWrap $! (2*3^20000)           -- twice prime to large power
-    , bench "10000000000000046"   $ nf cyclicWrap $! (10^16 + 46)          -- twice large prime
-    , bench "224403121196654400"  $ nf cyclicWrap $! (224403121196654400)  -- highly composite
+    [ bench "3^20000"             $ nf cyclicWrap (3^20000)             -- prime to large power
+    , bench "10000000000000061"   $ nf cyclicWrap (10^16 + 61)          -- large prime
+    , bench "2*3^20000"           $ nf cyclicWrap (2*3^20000)           -- twice prime to large power
+    , bench "10000000000000046"   $ nf cyclicWrap (10^16 + 46)          -- twice large prime
+    , bench "224403121196654400"  $ nf cyclicWrap (224403121196654400)  -- highly composite
     ]
   , bgroup "check prim roots"
     [ bench "3^20000"             $ nf (primRootWrap  3              20000) 2 -- prime to large power
