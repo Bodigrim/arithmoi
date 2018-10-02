bdc937a79d3593c70a3e79fe402141687a6863c6
Bhavik Mehta <bhavikmehta8@gmail.com>
2018-10-02 15:13:30 +0100

    Added more dirichlet tests

--- test-suite/Math/NumberTheory/DirichletCharactersTests.hs
+++ test-suite/Math/NumberTheory/DirichletCharactersTests.hs
@@ -10,6 +10,7 @@
 --
 
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE Rank2Types #-}
 
 module Math.NumberTheory.DirichletCharactersTests where
 
@@ -20,8 +21,10 @@ import Data.Ratio
 import Numeric.Natural
 import Data.Semigroup
 import Data.Complex
+import Data.List (nub, genericLength, genericReplicate)
+import Data.Maybe (mapMaybe)
 
-import GHC.TypeNats.Compat (SomeNat(..), someNatVal)
+import GHC.TypeNats.Compat (SomeNat(..), someNatVal, KnownNat)
 
 import Math.NumberTheory.ArithmeticFunctions (totient)
 import Math.NumberTheory.DirichletCharacters
@@ -34,29 +37,42 @@ rootOfUnityTest n (Positive d) = toComplex ((d `div` gcd n d) `stimes` toRootOfU
 -- | This tests property 6 from https://en.wikipedia.org/wiki/Dirichlet_character#Axiomatic_definition
 dirCharOrder :: Positive Natural -> Natural -> Bool
 dirCharOrder (Positive n) i = case someNatVal n of
-                                SomeNat (Proxy :: Proxy n) -> (totient n) `stimes` chi == trivialChar
+                                SomeNat (Proxy :: Proxy n) -> (totient n) `stimes` chi == principalChar
                                   where chi = fromIndex (i `mod` (totient n)) :: DirichletCharacter n
 
 -- | Tests wikipedia's property 3 (note 1,2,5 are essentially enforced by the type system).
-dirCharMultiplicative :: Positive Natural -> Natural -> Natural -> Natural -> Bool
-dirCharMultiplicative (Positive n) i a b = case someNatVal n of
-                                             SomeNat (Proxy :: Proxy n) -> let chiAchiB = (<>) <$> chi' a' <*> chi' b'
-                                                                               chiAB = chi' (a'*b')
-                                                                            in chiAB == chiAchiB
-                                               where chi = fromIndex (i `mod` (totient n)) :: DirichletCharacter n
-                                                     chi' = generalEval chi
-                                                     a' = fromIntegral a
-                                                     b' = fromIntegral b
+testMultiplicative :: KnownNat n => DirichletCharacter n -> Natural -> Natural -> Bool
+testMultiplicative chi a b = chiAB == chiAchiB
+  where chi' = generalEval chi
+        a' = fromIntegral a
+        b' = fromIntegral b
+        chiAB = chi' (a'*b')
+        chiAchiB = (<>) <$> chi' a' <*> chi' b'
 
-dirCharAtOne :: Positive Natural -> Natural -> Bool
-dirCharAtOne (Positive n) i = case someNatVal n of
-                                SomeNat (Proxy :: Proxy n) -> evaluate chi mempty == mempty
-                                  where chi = fromIndex (i `mod` (totient n)) :: DirichletCharacter n
+testAtOne :: KnownNat n => DirichletCharacter n -> Bool
+testAtOne chi = evaluate chi mempty == mempty
+
+dirCharProperty :: (forall n. KnownNat n => DirichletCharacter n -> a) -> Positive Natural -> Natural -> a
+dirCharProperty test (Positive n) i = case someNatVal n of
+                                        SomeNat (Proxy :: Proxy n) -> test chi
+                                          where chi = fromIndex (i `mod` (totient n)) :: DirichletCharacter n
+
+countCharacters :: Positive Natural -> Bool
+countCharacters (Positive n) = case someNatVal n of
+                                 SomeNat (Proxy :: Proxy n) ->
+                                   genericLength (nub [minBound :: DirichletCharacter n .. maxBound]) == totient n
+
+principalCase :: Positive Natural -> Bool
+principalCase (Positive n) = case someNatVal n of
+                             SomeNat (Proxy :: Proxy n) -> mapMaybe (generalEval chi) [minBound..maxBound] == genericReplicate (totient n) mempty
+                               where chi = principalChar :: DirichletCharacter n
 
 testSuite :: TestTree
 testSuite = testGroup "DirichletCharacters"
   [ testSmallAndQuick "RootOfUnity contains roots of unity" rootOfUnityTest
-  , testSmallAndQuick "Dirichlet characters have the right order" dirCharOrder
-  , testSmallAndQuick "Dirichlet characters are multiplicative" dirCharMultiplicative
-  , testSmallAndQuick "Dirichlet characters are 1 at 1" dirCharMultiplicative
+  , testSmallAndQuick "Dirichlet characters divide the right order" dirCharOrder
+  , testSmallAndQuick "Dirichlet characters are multiplicative" (dirCharProperty testMultiplicative)
+  , testSmallAndQuick "Dirichlet characters are 1 at 1" (dirCharProperty testAtOne)
+  , testSmallAndQuick "Right number of Dirichlet characters" countCharacters
+  , testSmallAndQuick "Principal character behaves as expected" principalCase
   ]
