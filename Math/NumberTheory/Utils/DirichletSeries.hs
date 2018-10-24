752bb52527701f3f09441537762aee4ea8f93b33
Bodigrim <andrew.lelechenko@gmail.com>
2018-10-24 21:54:10 +0100

    Represent Dirichlet series with maps

--- Math/NumberTheory/Utils/DirichletSeries.hs
+++ Math/NumberTheory/Utils/DirichletSeries.hs
@@ -8,48 +8,64 @@
 --
 
 {-# LANGUAGE ScopedTypeVariables   #-}
+{-# LANGUAGE TypeApplications      #-}
+{-# LANGUAGE ViewPatterns          #-}
 
 module Math.NumberTheory.Utils.DirichletSeries
   ( DirichletSeries
   , fromDistinctAscList
   , last
   , filter
+  , partition
+  , unions
+  , union
+  , size
   , timesAndCrop
   ) where
 
-import Prelude hiding (filter, last, rem, quot)
+import Prelude hiding (filter, last, rem, quot, snd)
+import Data.Coerce
 import qualified Data.List as L
 import Data.Semiring (Semiring(..))
 
 import Math.NumberTheory.Euclidean
 
+import Data.Map (Map)
+import qualified Data.Map as M
+
 -- Sparse Dirichlet series are represented by an ascending list of pairs.
 -- For instance, [(a, b), (c, d)] represents 1 + b/s^a + d/s^c.
-newtype DirichletSeries a b = DirichletSeries { unDirichletSeries :: [(a, b)] }
+newtype DirichletSeries a b = DirichletSeries { unDirichletSeries :: Map a b }
   deriving (Show)
 
-fromDistinctAscList :: [(a, b)] -> DirichletSeries a b
-fromDistinctAscList = DirichletSeries
+fromDistinctAscList :: forall a b. [(a, b)] -> DirichletSeries a b
+fromDistinctAscList = coerce (M.fromDistinctAscList @a @b)
 
--- TODO: represent Dirichlet series as a descending list of pairs.
--- This will allow to avoid traversal in L.last.
 last :: (Eq a, Num a, Semiring b) => a -> DirichletSeries a b -> b
-last 1 (DirichletSeries [(1, b)]) = one `plus` b
-last 1 _ = one
-last _ (DirichletSeries []) = zero
-last a' (DirichletSeries xs) = case L.last xs of
-  (a, b) -> if a == a' then b else zero
+last 1 (DirichletSeries m)
+  | [(1, b)] <- M.assocs m = one `plus` b
+  | otherwise = one
+last a' (DirichletSeries xs) = case M.maxViewWithKey xs of
+  Nothing -> zero
+  Just ((a, b), _) -> if a == a' then b else zero
 
-filter :: (a -> Bool) -> DirichletSeries a b -> DirichletSeries a b
-filter predicate (DirichletSeries xs) = DirichletSeries $ L.filter (predicate . fst) xs
+filter :: forall a b. (a -> Bool) -> DirichletSeries a b -> DirichletSeries a b
+filter predicate = coerce (M.filterWithKey @a @b (\k _ -> predicate k))
 
-merge :: (Ord a, Semiring b) => [(a, b)] -> [(a, b)] -> [(a, b)]
-merge xs [] = xs
-merge [] ys = ys
-merge xs'@(x'@(x, fx) : xs) ys'@(y'@(y, fy) : ys) = case x `compare` y of
-  LT -> x' : merge xs ys'
-  EQ -> (x, fx `plus` fy) : merge xs ys
-  GT -> y' : merge xs' ys
+partition :: forall a b. (a -> Bool) -> DirichletSeries a b -> (DirichletSeries a b, DirichletSeries a b)
+partition predicate = coerce (M.partitionWithKey @a @b (\k _ -> predicate k))
+
+unions :: forall a b. (Ord a, Semiring b) => [DirichletSeries a b] -> DirichletSeries a b
+unions = coerce (M.unionsWith @[] @a @b plus)
+
+union :: forall a b. (Ord a, Semiring b) => DirichletSeries a b -> DirichletSeries a b -> DirichletSeries a b
+union = coerce (M.unionWith @a @b plus)
+
+merge :: (Ord a, Semiring b) => Map a b -> Map a b -> Map a b
+merge = M.unionWith plus
+
+size :: forall a b. DirichletSeries a b -> Int
+size = coerce (M.size @a @b)
 
 -- | Precondition: all pairwise products of keys are distinct.
 -- In other words, no element repeats in the list
@@ -60,25 +76,25 @@ timesAndCrop
   -> DirichletSeries a b -- ^ longer series
   -> DirichletSeries a b -- ^ shorter series
   -> DirichletSeries a b
-timesAndCrop n (DirichletSeries as) (DirichletSeries [(b, fb)])
+timesAndCrop n (DirichletSeries as) (DirichletSeries (M.assocs -> [(b, fb)]))
   = DirichletSeries
-  -- TODO: fuse generation of as' and its merge with as
-  -- into one function, maintaining two pointers to as.
-  -- This will avoid generation of intermediate structures.
-  $ [(b, fb)] `merge` (as `merge` as')
+  $ M.insertWith plus b fb (as `merge` as')
   where
     nb = n `quot` b
     as'
-      = map (\(a, fa) -> (a * b, fa `times` fb))
+      = M.fromDistinctAscList
+      $ map (\(a, fa) -> (a * b, fa `times` fb))
       $ L.filter (\(a, _) -> nb `rem` a == 0)
-      -- $ L.takeWhile (\(a, _) -> a <= nb)
+      $ M.assocs
+      $ (\(lt, eq, _) -> maybe lt (\v -> M.insert nb v lt) eq)
+      $ M.splitLookup nb
       $ as
 timesAndCrop n (DirichletSeries as) (DirichletSeries bs)
   = DirichletSeries
   $ foldl merge (as `merge` bs)
   $ map
-    (\(b, fb) -> L.filter (\(ab, _) -> n `rem` ab == 0) $ map
+    (\(b, fb) -> M.fromDistinctAscList $ L.filter (\(ab, _) -> n `rem` ab == 0) $ map
       (\(a, fa) -> (a * b, fa `times` fb))
-      as)
-    bs
-{-# SPECIALISE timesAndCrop :: Semiring b => (Integer -> Bool) -> DirichletSeries Integer b -> DirichletSeries Integer b -> DirichletSeries Integer b #-}
+      (M.assocs as))
+    (M.assocs bs)
+{-# SPECIALISE timesAndCrop :: Semiring b => Integer -> DirichletSeries Integer b -> DirichletSeries Integer b -> DirichletSeries Integer b #-}
