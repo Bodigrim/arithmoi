b13efca1b476540afd82cd70b8d731c5bde50f12
Alexandre Baldé <alexandrer_b@outlook.com>
2018-10-27 00:47:35 +0200

    [#141] Add test to square-free number distribution

--- test-suite/Math/NumberTheory/ArithmeticFunctionsTests.hs
+++ test-suite/Math/NumberTheory/ArithmeticFunctionsTests.hs
@@ -26,6 +26,7 @@ import qualified Data.IntSet as IS
 import Math.NumberTheory.ArithmeticFunctions
 import Math.NumberTheory.Primes (UniqueFactorisation (factorise))
 import Math.NumberTheory.TestUtils
+import Math.NumberTheory.Zeta (zetas)
 
 import Numeric.Natural
 
@@ -279,6 +280,13 @@ nFreedomProperty2 :: NonZero Word -> NonNegative Int -> Bool
 nFreedomProperty2 (NonZero n) (NonNegative m) =
     all (isNFree n) $ take m (nFrees n :: [Integer])
 
+nFreedomProperty3 :: Positive Int -> Bool
+nFreedomProperty3 (Positive m) =
+    let zet = 1 / zetas 1e-14 !! 2 :: Double
+        m' = 100 * m
+        sqfree = fromIntegral m' / fromIntegral (last (take m' $ nFrees 2 :: [Integer]))
+    in 1 / fromIntegral m >= abs (zet - sqfree)
+
 nFreedomAssertion1 :: Assertion
 nFreedomAssertion1 =
     assertEqual "1 is the sole 0-free number" (nFrees 0) ([1] :: [Int])
@@ -346,6 +354,7 @@ testSuite = testGroup "ArithmeticFunctions"
   , testGroup "N-freedom"
     [ testSmallAndQuick "`isNFree` matches the definition" nFreedomProperty1
     , testSmallAndQuick "numbers produces by `nFrees`s are `n`-free" nFreedomProperty2
+    , testSmallAndQuick "distribution of square-free numbers matches expected" nFreedomProperty3
     , testCase "`1` is the only 0-free number" nFreedomAssertion1
     , testCase "`1` is the only 1-free number" nFreedomAssertion2
     ]
