56139c916dc12eacfa7694a91a62fec5a33b56cc
Bodigrim <andrew.lelechenko@gmail.com>
2019-03-27 18:10:02 +0000

    Restore Math.NumberTheory.UniqueFactorisation

---
+++ Math/NumberTheory/UniqueFactorisation.hs
@@ -0,0 +1,13 @@
+-- |
+-- Module:      Math.NumberTheory.Recurrencies
+-- Description: Deprecated
+-- Copyright:   (c) 2019 Andrew Lelechenko
+-- Licence:     MIT
+-- Maintainer:  Andrew Lelechenko <andrew.lelechenko@gmail.com>
+--
+
+module Math.NumberTheory.UniqueFactorisation {-# DEPRECATED "Use `Math.NumberTheory.Primes` instead." #-}
+    ( module Math.NumberTheory.Primes
+    ) where
+
+import Math.NumberTheory.Primes
