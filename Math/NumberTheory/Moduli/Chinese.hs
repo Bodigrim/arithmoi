0079b1482038aba8273ad65ff4203a7030209b7e
Bodigrim <andrew.lelechenko@gmail.com>
2019-11-01 22:23:49 +0000

    Use Data.Euclidean.gcdExt for chinese remainders

--- Math/NumberTheory/Moduli/Chinese.hs
+++ Math/NumberTheory/Moduli/Chinese.hs
@@ -31,16 +31,17 @@ module Math.NumberTheory.Moduli.Chinese
   , chineseRemainder2
   ) where
 
-import Prelude hiding (rem, quot, gcd, lcm)
+import Prelude hiding ((^), rem, mod, quot, gcd, lcm)
+import qualified Prelude
 
 import Control.Monad (foldM)
+import Data.Euclidean
 import Data.Foldable
 import Data.Ratio
+import Data.Semiring (Semiring(..), (^), Ring, minus)
 import GHC.TypeNats.Compat
-import Numeric.Natural
 
 import Math.NumberTheory.Moduli.Class
-import Math.NumberTheory.Euclidean
 import Math.NumberTheory.Euclidean.Coprimes
 import Math.NumberTheory.Utils (recipMod, splitOff)
 
@@ -54,17 +55,17 @@ import Math.NumberTheory.Utils (recipMod, splitOff)
 -- Just 5
 -- >>> chineseCoprime (3, 4) (5, 6)
 -- Nothing -- moduli must be coprime
-chineseCoprime :: (Integral a, Euclidean a) => (a, a) -> (a, a) -> Maybe a
-chineseCoprime (n1, m1) (n2, m2) = case d of
-  1 -> Just $ ((1 - u * m1) * n1 + (1 - v * m2) * n2) `mod` (m1 * m2)
-  _ -> Nothing
+chineseCoprime :: (Eq a, Ring a, Euclidean a) => (a, a) -> (a, a) -> Maybe a
+chineseCoprime (n1, m1) (n2, m2)
+  | d == one
+  = Just $ ((one `minus` u `times` m1) `times` n1 `plus` (one `minus` v `times` m2) `times` n2) `rem` (m1 `times` m2)
+  | otherwise = Nothing
   where
     (d, u, v) = extendedGCD m1 m2
 
 {-# SPECIALISE chineseCoprime :: (Int, Int) -> (Int, Int) -> Maybe Int #-}
 {-# SPECIALISE chineseCoprime :: (Word, Word) -> (Word, Word) -> Maybe Word #-}
 {-# SPECIALISE chineseCoprime :: (Integer, Integer) -> (Integer, Integer) -> Maybe Integer #-}
-{-# SPECIALISE chineseCoprime :: (Natural, Natural) -> (Natural, Natural) -> Maybe Natural #-}
 
 -- | 'chinese' @(n1, m1)@ @(n2, m2)@ returns @n@ such that
 -- @n \`mod\` m1 == n1@ and @n \`mod\` m2 == n2@, if exists.
@@ -76,10 +77,10 @@ chineseCoprime (n1, m1) (n2, m2) = case d of
 -- Just 11
 -- >>> chinese (3, 4) (2, 6)
 -- Nothing
-chinese :: forall a. (Integral a, GcdDomain a, Euclidean a) => (a, a) -> (a, a) -> Maybe a
+chinese :: forall a. (Eq a, Ring a, Euclidean a) => (a, a) -> (a, a) -> Maybe a
 chinese (n1, m1) (n2, m2)
-  | (n1 - n2) `rem` g == 0
-  = chineseCoprime (n1 `mod` m1', m1') (n2 `mod` m2', m2')
+  | (n1 `minus` n2) `rem` g == zero
+  = chineseCoprime (n1 `rem` m1', m1') (n2 `rem` m2', m2')
   | otherwise
   = Nothing
   where
@@ -90,14 +91,14 @@ chinese (n1, m1) (n2, m2)
     ms = unCoprimes $ splitIntoCoprimes [(m1, 1), (m2 `quot` g, 1)]
 
     m1', m2' :: a
-    (m1', m2') = foldl' go (1, 1) $ map fst ms
+    (m1', m2') = foldl' go (one, one) $ map fst ms
 
     go :: (a, a) -> a -> (a, a)
     go (t1, t2) p
       | k1 <= k2
-      = (t1, t2 * p ^ k2)
+      = (t1, t2 `times` p ^ k2)
       | otherwise
-      = (t1 * p ^ k1, t2)
+      = (t1 `times` p ^ k1, t2)
       where
         (k1, _) = splitOff p m1
         (k2, _) = splitOff p m2
@@ -105,7 +106,6 @@ chinese (n1, m1) (n2, m2)
 {-# SPECIALISE chinese :: (Int, Int) -> (Int, Int) -> Maybe Int #-}
 {-# SPECIALISE chinese :: (Word, Word) -> (Word, Word) -> Maybe Word #-}
 {-# SPECIALISE chinese :: (Integer, Integer) -> (Integer, Integer) -> Maybe Integer #-}
-{-# SPECIALISE chinese :: (Natural, Natural) -> (Natural, Natural) -> Maybe Natural #-}
 
 isCompatible :: KnownNat m => Mod m -> Rational -> Bool
 isCompatible n r = case invertMod (fromInteger (denominator r)) of
@@ -179,7 +179,7 @@ chineseRemainder remainders = foldM addRem 0 remainders
     addRem acc (r,m) = do
         let cf = modulus `quot` m
         inv <- recipMod cf m
-        Just $! (acc + inv*cf*r) `mod` modulus
+        Just $! (acc + inv*cf*r) `rem` modulus
 
 -- | @chineseRemainder2 (r_1,m_1) (r_2,m_2)@ calculates the solution of
 --
@@ -188,6 +188,15 @@ chineseRemainder remainders = foldM addRem 0 remainders
 --
 --   if @m_1@ and @m_2@ are coprime.
 chineseRemainder2 :: (Integer, Integer) -> (Integer, Integer) -> Integer
-chineseRemainder2 (n1, m1) (n2, m2) = ((1 - u * m1) * n1 + (1 - v * m2) * n2) `mod` (m1 * m2)
+chineseRemainder2 (n1, m1) (n2, m2) = ((1 - u * m1) * n1 + (1 - v * m2) * n2) `Prelude.mod` (m1 * m2)
   where
     (_, u, v) = extendedGCD m1 m2
+
+-------------------------------------------------------------------------------
+-- Utils
+
+extendedGCD :: (Eq a, Ring a, Euclidean a) => a -> a -> (a, a, a)
+extendedGCD a b = (g, s, t)
+  where
+    (g, s) = gcdExt a b
+    t = (g `minus` a `times` s) `quot` b
