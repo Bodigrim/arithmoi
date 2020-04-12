72ce88f201460c607d481ec9ca1be442ddbf29ae
folidota <federico.bongiorno14@imperial.ac.uk>
2020-04-13 00:06:01 +0100

    Addressed comments on pattern matching

--- Math/NumberTheory/Moduli/CubicSymbol.hs
+++ Math/NumberTheory/Moduli/CubicSymbol.hs
@@ -1,15 +1,16 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Math.NumberTheory.Moduli.CubicSymbol
     ( CubicSymbol(..)
     , conj
     , cubicSymbol
     ) where
 
-import Math.NumberTheory.Quadratic.EisensteinIntegers
 import qualified Math.NumberTheory.Quadratic.EisensteinIntegers as E
-
 import qualified Math.NumberTheory.Primes as P
 import qualified Data.Euclidean as A
-import Data.Semigroup as S
+import qualified Data.Semigroup as S
+import qualified Data.List as L
 
 
 
@@ -19,52 +20,52 @@ instance S.Semigroup CubicSymbol where
     (<>) = multiplication
 
 instance Show CubicSymbol where
-    show x
-        | x == Zero         = show 0
-        | x == Omega        = "ω"
-        | x == OmegaSquare  = "ω²"
-        | x == One          = show 1
+    show = \case
+        Zero         -> show 0
+        Omega        -> "ω"
+        OmegaSquare  -> "ω²"
+        One          -> show 1
 
 
 multiplication :: CubicSymbol -> CubicSymbol -> CubicSymbol
-multiplication x y
-    | x == Zero || y == Zero                   = Zero
-    | x == One                                 = y
-    | y == One                                 = x
-    | x == Omega && y == Omega                 = OmegaSquare
-    | x == Omega && y == OmegaSquare           = One
-    | x == OmegaSquare && y == Omega           = One
-    | x == OmegaSquare && y == OmegaSquare     = Omega
-    | otherwise                                = error ""
+multiplication Zero _ = Zero
+multiplication _ Zero = Zero
+multiplication One y = y
+multiplication x One = x
+multiplication Omega Omega = OmegaSquare
+multiplication Omega OmegaSquare = One
+multiplication OmegaSquare Omega = One
+multiplication OmegaSquare OmegaSquare = Omega
+
+
 
 exponentiation :: CubicSymbol -> Integer -> CubicSymbol
-exponentiation symbol exponent
-    | symbol == Zero || symbol == One          = symbol
-    | remainder == 0                           = One
-    | symbol == Omega && remainder == 1        = Omega
-    | symbol == Omega && remainder == 2        = OmegaSquare
-    | symbol == OmegaSquare && remainder == 1  = OmegaSquare
-    | symbol == OmegaSquare && remainder == 1  = Omega
-    | otherwise                                = error ""
-        where remainder = exponent `rem` 3
+exponentiation Zero _ = Zero
+exponentiation One _ = One
+exponentiation _ 0 = One
+exponentiation Omega 1 = Omega
+exponentiation Omega 2 = OmegaSquare
+exponentiation OmegaSquare 1 = OmegaSquare
+exponentiation OmegaSquare 2 = Omega
+
+
 
 conj :: CubicSymbol -> CubicSymbol
-conj x
-    | x == Zero || x == One      = x
-    | x == Omega                 = OmegaSquare
-    | x == OmegaSquare           = Omega
-    | otherwise                  = error ""
-
-
+conj = \case
+    Zero          -> Zero
+    Omega         -> OmegaSquare
+    OmegaSquare   -> Omega
+    One           -> One
 
 -- Converts Eisenstein integer to CubicSymbol value.
 toCubicSymbol :: E.EisensteinInteger -> CubicSymbol
-toCubicSymbol x
-    | x == 0               = Zero
-    | x == 0 E.:+ 1        = Omega
-    | x == (-1) E.:+ (-1)  = OmegaSquare
-    | x == 1               = One
-    | otherwise            = error ""
+toCubicSymbol = \case
+    0               -> Zero
+    0 E.:+ 1        -> Omega
+    (-1) E.:+ (-1)  -> OmegaSquare
+    1               -> One
+
+
 
 
 cubicSymbol :: E.EisensteinInteger -> E.EisensteinInteger -> CubicSymbol
@@ -76,8 +77,9 @@ cubicSymbol alpha n = Prelude.foldr (g) One factorisation
 
 
 primePowerCubicSymbol :: E.EisensteinInteger -> (P.Prime E.EisensteinInteger, Word) -> CubicSymbol
-primePowerCubicSymbol alpha (primeNumber, wordExponent) = exponentiation symbol exponent
-    where exponent = Prelude.fromIntegral wordExponent
+primePowerCubicSymbol alpha (primeNumber, wordExponent) = exponentiation symbol remainder
+    where remainder = exponent `rem` 3
+          exponent = Prelude.fromIntegral wordExponent
           symbol =  primeCubicSymbol alpha prime
           prime = P.unPrime primeNumber
 
@@ -94,7 +96,7 @@ primeCubicSymbol alpha prime
               residue = Prelude.foldr (f) 1 listOfAlphas
               f = \x y -> A.rem (x * y) prime
               -- The function @take@ does not accept Integer values.
-              listOfAlphas = [alpha | index <- [1..alphaExponent]]
+              listOfAlphas = L.genericReplicate alphaExponent alpha
               -- Exponent is defined to be 1/3*(@primeNorm@ - 1).
               alphaExponent = primeNorm `div` 3
               
