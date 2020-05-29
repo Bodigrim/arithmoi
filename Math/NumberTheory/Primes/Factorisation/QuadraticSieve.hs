e98ab096943d2680f16ad8adb9fcc6dc71f28c89
folidota <federico.bongiorno14@imperial.ac.uk>
2020-05-29 21:38:40 +0100

    Added functions to quadratic sieve. It does not compile

--- Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
+++ Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
@@ -5,10 +5,19 @@ module Math.NumberTheory.Primes.Factorisation.QuadraticSieve
 import Control.Monad
 import Control.Monad.ST
 import qualified Data.Vector as V
+import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Mutable as MV
+import qualified Data.Vector.Unboxed.Mutable as UMV
+import Math.NumberTheory.Primes
 import Math.NumberTheory.Primes.Sieve.SmoothSieve
-import Math.NumberTheory.Roots
+import Math.NumberTheory.Primes.Sieve.Eratosthenes
+import Math.NumberTheory.Moduli.JacobiSymbol
 
-quadraticSieve :: Int -> Int -> (Int, Int)
+quadraticSieve :: Integer -> Int -> U.Vector Int
 -- Input assumed odd composite positive integer
-quadraticSieve n b = (0, 0)
+quadraticSieve n b = goodPrimes
+    where
+        -- Remove 2
+        vectorOfPrimes = V.fromList (tail listOfPrimes)
+        listOfPrimes = map (fromIntegral . unPrime) (primeList (primeSieve (toInteger b)))
+        goodPrimes = U.filter (\x -> (jacobi n x == One)) vectorOfPrimes
