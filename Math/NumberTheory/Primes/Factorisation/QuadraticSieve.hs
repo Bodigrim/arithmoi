2ae1bd983f7c9bcde535d839af594413012646fc
folidota <federico.bongiorno14@imperial.ac.uk>
2020-06-02 18:10:14 +0100

    Quadratic Sieve up to linear algebra

--- Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
+++ Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE ViewPatterns #-}
+
 module Math.NumberTheory.Primes.Factorisation.QuadraticSieve
   ( quadraticSieve
   ) where
 
 import Control.Monad
 import Control.Monad.ST
+import Data.Maybe
 import qualified Data.Vector as V
 --import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Mutable as MV
@@ -13,7 +16,7 @@ import qualified Data.IntSet as S
 import Math.NumberTheory.Roots
 import Math.NumberTheory.Primes
 --import Math.NumberTheory.Primes.Sieve.SmoothSieve
-import Math.NumberTheory.Moduli.JacobiSymbol
+--import Math.NumberTheory.Moduli.JacobiSymbol
 import Math.NumberTheory.Moduli.Sqrt
 import Math.NumberTheory.Utils.FromIntegral
 
@@ -21,69 +24,71 @@ import Math.NumberTheory.Utils.FromIntegral
 -- the Quadratic Sieve attempt to decompose n into smaller factors p and q.
 -- Carefully check conversion bounds. Need to make sure Integer does not get
 -- too big in order to meaningfully convert to Int. t cannot be too large.
-quadraticSieve :: Integer -> Integer -> Int -> (V.Vector (Integer, S.IntSet), V.Vector [Integer])
+quadraticSieve :: Integer -> Int -> Int -> V.Vector S.IntSet
 quadraticSieve n b t = runST $ do
-    let factorBase = findFactorBase n b
-        modularSquareRoots = V.map (\x -> sqrtsModPrime n x) factorBase
+    let factorBase = [nextPrime 2..precPrime b]
         -- This is not the ceiling of the square root.
         squareRoot = (integerSquareRoot n) + 1
         -- This algorithm takes the input Integer n and the Int parametre
         -- t and returns the sieving internal (x^2 - n, factorisation) as x goes
         -- from the square root of n to t.
         sievingInterval = V.generate t (\index -> (reduce index, S.empty))
-        reduce i = (squareRoot + (toInteger i)) ^ (2 :: Int) - n
+        reduce i = integerToInt ((squareRoot + (intToInteger i)) ^ (2 :: Int) - n)
     sievingIntervalM <- V.thaw sievingInterval
-    smoothSieveM sievingIntervalM factorBase modularSquareRoots squareRoot
+    smoothSieveM sievingIntervalM factorBase n
     sievingIntervalF <- V.unsafeFreeze sievingIntervalM
-    pure (sievingIntervalF, modularSquareRoots)
-
--- This algorithm takes n and b and returns the factor base, an unboxed
--- vector of Int. This comprises all the odd primes less than b whose
--- quadratic residue with n is One.
--- Could this algorithm be faster? Do we need to check each prime one
--- by one or can we run a sieveBlock?
-findFactorBase :: Integer -> Integer -> V.Vector (Prime Integer)
-findFactorBase n b = factorBase
-    where
-        -- in jacobi, both arguments have to be of Integer type.
-        -- Is there a faster way not to check 2?
-        factorBase = V.filter (\x -> (unPrime x == 2) || (jacobi n (unPrime x) == One)) vectorOfPrimes
-        -- Remove 2 from list of Primes
-        vectorOfPrimes = V.fromList listOfPrimes
-        listOfPrimes = [nextPrime 2..precPrime b]
+    let indexedFactorisations = findSmoothNumbers sievingIntervalF
+        -- This is to acces indexedSmoothNumbers aftwerwards. There is
+        -- probably a better way to remember vector
+        factorisations = snd indexedFactorisations
+    factorisationsM <- V.thaw factorisations
+    --gaussianElimination factorisationsM
+    factorisationsF <- V.unsafeFreeze factorisationsM
+    -- let perfectCombination = linearSolve factorisations
+    pure factorisationsF
 
 -- This algorithm takes the sievingInterval, the factorBase and the
 -- modularSquareRoots and returns the smooth numbers in the interval with
--- respect to the factorBase ogether with their factorisations.
-smoothSieveM :: MV.MVector s (Integer, S.IntSet) -> V.Vector (Prime Integer) -> V.Vector [Integer] -> Integer -> ST s ()
-smoothSieveM sievingIntervalM factorBase modularSquareRoots squareRoot = do
-    -- Is it better to pass t as an argument rather than computing it again?
+-- respect to the factorBase together with their factorisations.
+smoothSieveM :: MV.MVector s (Int, S.IntSet) -> [Prime Int] -> Integer -> ST s ()
+smoothSieveM sievingIntervalM factorBase n = do
     let t = MV.length sievingIntervalM
-        s = V.length factorBase
-    -- Loops through factorBase remembering index
-    forM_ [0..(s - 1)] $ \indexOfPrime -> do
-        let primeInteger = unPrime (factorBase V.! indexOfPrime)
-            -- Is this necessary. Can just convert later directly with squareRoot
-            residue = squareRoot `mod` primeInteger
-            prime = integerToInt primeInteger
-        forM_ (modularSquareRoots V.! indexOfPrime) $ \modularSquareRoot -> do
-            -- What is the best way for reducing modulo the prime?
-            let startingIndex = integerToInt (reduceModuloPrime (modularSquareRoot - residue) primeInteger)
-                -- This function should be implemented somewhere else in arithmoi
-                reduceModuloPrime x p = if (x >= 0) then (x `mod` p) else (p + (x `mod` p))
-            forM_ [startingIndex, startingIndex + prime..(t - 1)] $ \entry -> do
-                -- Better way to write function on tuple
-                -- See if it's better to insert Prime or indexOfPrime
-                let change tuple = ((fst tuple) `div` primeInteger, S.insert indexOfPrime (snd tuple))
+        squareRoot = (integerSquareRoot n) + 1
+    forM_ factorBase $ \(prime) -> do
+        let modularSquareRoots = map integerToInt (sqrtsModPrime n ((fromJust . toPrimeIntegral) prime))
+        forM_ modularSquareRoots $ \modularSquareRoot -> do
+            let startingIndex = integerToInt ((intToInteger modularSquareRoot - squareRoot) `mod` (intToInteger . unPrime) prime)
+            forM_ [startingIndex, startingIndex + (unPrime prime)..(t - 1)] $ \entry -> do
+                let change (y, is) = (y `div` unPrime prime, S.insert (unPrime prime) is)
                 MV.modify sievingIntervalM change entry
 
--- convertToBinary :: V.Vector (IntMap Int) -> V.Vector IntSet
--- convertToBinary facorisations = binaryFactorisations
---
--- -- This algorithm takes the factorisations of the smooth numbers reduced
--- -- modulo 2 and returns a combitation which gives a prefect square
--- linearSolve :: V.Vector IntSet -> IntSet
--- linearSolve binaryFactorisations = pefectCombination
+-- This function returns the smooth numbers together with their index in order
+-- to retrieve later the value of x and x^2 - n.
+-- This function should be better written.
+findSmoothNumbers :: V.Vector (Int, S.IntSet) -> (V.Vector Int, V.Vector S.IntSet)
+findSmoothNumbers sievingIntervalF = V.unzip smoothTuples
+     where
+         smoothTuples = V.map (\(a, (_, b)) -> (a, b)) indexedSmooth
+         indexedSmooth = V.filter (\(_, (s, _)) -> (s == 1)) indexedSieving
+         indexedSieving = V.zip (V.generate t id) sievingIntervalF
+         t = V.length sievingIntervalF
+
+-- This algorithm takes the factorisations of the smooth numbers reduced
+-- modulo 2 and returns a combitation which gives a prefect square
+-- gaussianElimination :: MV.MVector s S.IntSet -> ST s ()
+-- gaussianElimination factorisations = do
+--     -- This is to remember the index of the factorisation
+--     let s = MV.length factorisations
+--     forM_ [0..(s - 1)] $ \indexFactorisation -> do
+--         primeFactorisation <- MV.read factorisations indexFactorisation
+--         -- Consider case primeFactorisation is empty
+--         let () = S.partition
+--         -- Delete entries in same column
+--         MV.write factorisations indexFactorisation (S.singleton firstPrime)
+--         -- Delete entries in further columns
+--         forM_ [indexFactorisation..(s - 1)] $ \column -> do
+
+
 --
 -- findFirstSquare :: U.Vector Int -> IntSet -> Integer
 -- findFirstSquare factorBase perfectCombination = w
