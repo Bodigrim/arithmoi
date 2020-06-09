f9f963fee9c50455960f72bc004e5ff2c7061d20
folidota <federico.bongiorno14@imperial.ac.uk>
2020-06-09 19:33:27 +0100

    Added comments and hlint suggestions

--- Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
+++ Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
@@ -1,75 +1,73 @@
-{-# LANGUAGE ViewPatterns #-}
-{-# LANGUAGE TupleSections #-}
+--{-# LANGUAGE ViewPatterns #-}
+--{-# LANGUAGE TupleSections #-}
 
 module Math.NumberTheory.Primes.Factorisation.QuadraticSieve
   ( quadraticSieve
   , gaussianEliminationM
   ) where
 
+import qualified Data.Vector as V
+import qualified Data.Vector.Mutable as MV
+import qualified Data.IntMap as I
+import qualified Data.IntSet as S
+import qualified Math.NumberTheory.Primes.IntSet as SP
 import Control.Monad
 import Control.Monad.ST
 import Data.Maybe
---import Data.Semigroup
---import Data.Foldable
-import qualified Data.IntSet as S
-import qualified Data.IntMap as I
-import qualified Data.Vector as V
-import qualified Data.Vector.Mutable as MV
-
 import Math.NumberTheory.Roots
 import Math.NumberTheory.Primes
-import qualified Math.NumberTheory.Primes.IntSet as SP
 import Math.NumberTheory.Moduli.Sqrt
 import Math.NumberTheory.Utils.FromIntegral
-import Debug.Trace
+
 
 -- Given an odd positive composite Integer n and Int parametres b and t,
 -- the Quadratic Sieve attempt to decompose n into smaller factors p and q.
 -- Carefully check conversion bounds. Need to make sure Integer does not get
 -- too big in order to meaningfully convert to Int. t cannot be too large.
-
 quadraticSieve :: Integer -> Int -> Int -> Integer
 quadraticSieve n b t = runST $ do
     let factorBase = [nextPrime 2..precPrime b]
         -- This is not the ceiling of the square root.
-        squareRoot = (integerSquareRoot n) + 1
-        -- This algorithm takes the input Integer n and the Int parametre
-        -- t and returns the sieving internal (x^2 - n, factorisation) as x goes
-        -- from the square root of n to t.
-        --I.IntMap (Int)
+        squareRoot = integerSquareRoot n + 1
+        -- Generating sieving interval. This consists of tuples whose first
+        -- component is x^2 - n as x runs from the square root of n for a
+        -- total of length t. The second component stores the factorisation
+        -- modulo 2 as an IntSet.
         sievingInterval = V.generate t (\index -> (reduce index, SP.empty))
-        reduce i = integerToInt ((squareRoot + (intToInteger i)) ^ (2 :: Int) - n)
+        reduce i = integerToInt ((squareRoot + intToInteger i) ^ (2 :: Int) - n)
     sievingIntervalM <- V.thaw sievingInterval
+    -- Perform sieving
     smoothSieveM sievingIntervalM factorBase n
     sievingIntervalF <- V.unsafeFreeze sievingIntervalM
+    -- Filters smooth numbers
     let indexedFactorisations = findSmoothNumbers sievingIntervalF
     indexedFactorisationsM <- V.thaw indexedFactorisations
+    -- Solves linear equation
     gaussianEliminationM indexedFactorisationsM
     solutionRules <- V.unsafeFreeze indexedFactorisationsM
-    -- 1 is the parametre indexing all solution to the matrix. It is
-    -- recursively increased to go through all the solutions.
-    let r = length (V.findIndices (/= S.empty) (fmap fst solutionRules))
-    pure (trace ("Dimension of kernel: " ++ show r ++ "\nFactoring") $ findFactor n 1 (fmap fst solutionRules) indexedFactorisations)
+    -- Checks thorugh all possible solutions. That is 2 ^ (dim kernel)
+    pure (findFactor n 1 (fmap fst solutionRules) indexedFactorisations)
 
 -- This algorithm takes the sievingInterval, the factorBase and the
--- modularSquareRoots and returns the smooth numbers in the interval with
--- respect to the factorBase together with their factorisations.
+-- modularSquareRoots and divides by all the prime in the factor base
+-- storing the factorisations. The smooth numbers correspond to tuples
+-- whose first component is 1 and whose second component is their factorisation.
 smoothSieveM :: MV.MVector s (Int, SP.PrimeIntSet) -> [Prime Int] -> Integer -> ST s ()
-smoothSieveM sievingIntervalM factorBase n = trace ("Sieving") $ do
+smoothSieveM sievingIntervalM factorBase n = do
     let t = MV.length sievingIntervalM
-        squareRoot = (integerSquareRoot n) + 1
+        squareRoot = integerSquareRoot n + 1
     forM_ factorBase $ \prime -> do
         let modularSquareRoots = map integerToInt (sqrtsModPrime n ((fromJust . toPrimeIntegral) prime))
         forM_ modularSquareRoots $ \modularSquareRoot -> do
             let startingIndex = integerToInt ((intToInteger modularSquareRoot - squareRoot) `mod` (intToInteger . unPrime) prime)
-            forM_ [startingIndex, startingIndex + (unPrime prime)..(t - 1)] $ \entry -> do
+            forM_ [startingIndex, startingIndex + unPrime prime..(t - 1)] $ \entry -> do
                 let change (y, is) = (y `div` unPrime prime, SP.insert prime is)
                 MV.modify sievingIntervalM change entry
 
--- This function returns the smooth numbers together with their index in order
--- to retrieve later the value of x and x^2 - n.
--- This function should be better written.
--- Change it to V.Vector (Int, S.IntSet)
+-- This function returns the smooth numbers together with their index. This
+-- is in order to retrieve later the value of x and x^2 - n. The value stored
+-- in the first component of the tuple is a set whose only component is
+-- the index of column before sieving.
 findSmoothNumbers :: V.Vector (Int, SP.PrimeIntSet) -> V.Vector (S.IntSet, SP.PrimeIntSet)
 findSmoothNumbers = V.imapMaybe selectSmooth
      where
@@ -98,13 +96,17 @@ findSmoothNumbers = V.imapMaybe selectSmooth
 --
 --         gaussianElimination indexedFactorisationsM
 
+-- This solves the linear equation. It returns a vector of tuples. Its first
+-- component is either empty (when the corresponding column contains a pivot)
+-- or stores a basis element of the kernel of the matrix (when it corresponds
+-- to a free variable). The second component consits of empty vectors.
 gaussianEliminationM :: MV.MVector s (S.IntSet, SP.PrimeIntSet) -> ST s ()
-gaussianEliminationM indexedFactorisationsM = trace ("Reducing") $ do
+gaussianEliminationM indexedFactorisationsM = do
     let s = MV.length indexedFactorisationsM
-    -- Pivots remembers the matrix entry where the pivots are
+    -- Loop through columns
     forM_ [0..(s - 1)] $ \column -> do
         x@(_, primeFactorisation) <- MV.read indexedFactorisationsM column
-        -- If column is linearly dependent to the previous ones go to the next one
+        -- If column is linearly depends on the previous ones go to the next one
         when (primeFactorisation /= SP.empty) $ do
             -- otherwise find the new pivot
             let pivot = SP.findMin primeFactorisation
@@ -113,43 +115,14 @@ gaussianEliminationM indexedFactorisationsM = trace ("Reducing") $ do
                 (_, nextFactorisation) <- MV.read indexedFactorisationsM index
                 when (pivot `SP.member` nextFactorisation) $ do
                     let xor (a, u) (b, v) = ((a S.\\ b) <> (b S.\\ a), (u SP.\\ SP.unPrimeIntSet v) <> (v SP.\\ SP.unPrimeIntSet u))
+                    -- Remember combinations of columns summing to zero
                     MV.modify indexedFactorisationsM (xor x) index
 
-convertToSolution :: I.IntMap Bool -> V.Vector S.IntSet -> S.IntSet
-convertToSolution parametres solutionRules = V.ifoldr add S.empty solutionRules
-    where
-        add index rule previous = case (I.lookup index parametres) of
-            Just True  -> previous
-            Just False -> xor rule previous
-            Nothing    -> previous
-            where
-                xor a b = (a S.\\ b) <> (b S.\\ a)
-
-generateParametres :: V.Vector S.IntSet -> Integer -> I.IntMap Bool
-generateParametres _ 0 = I.empty
-generateParametres solutionRules generator = case (V.findIndex (/= S.empty) solutionRules) of
-    Just column -> I.insert column (even generator) (generateParametres (solutionRules V.// [(column, S.empty)]) (generator `div` 2))
-    Nothing     -> I.empty
-
-findFirstSquare :: S.IntSet -> Integer -> Integer
-findFirstSquare solution n = S.fold construct 1 solution
-    where
-        construct current previous = ((intToInteger current + squareRoot) * previous) `mod` n
-        squareRoot = (integerSquareRoot n) + 1
-
-findSecondSquare :: S.IntSet -> V.Vector (S.IntSet, SP.PrimeIntSet) -> Integer -> Integer
-findSecondSquare solution indexedFactorisations n = I.foldrWithKey computeRoot 1 countSquares
-    where
-        computeRoot key power previous = case (odd power) of
-            True  -> error "Linear algebra issue."
-            False -> ((intToInteger key) ^ (power `div` 2 :: Int) * previous) `mod` n
-        countSquares = V.foldl countPowers I.empty squares
-        countPowers acc factorisation = SP.foldr (\key im -> I.insertWith (+) (unPrime key) 1 im) acc factorisation
-        squares = V.mapMaybe selectSolution indexedFactorisations
-        selectSolution (index, factorisation)
-            | (index `S.isSubsetOf` solution) = Just factorisation
-            | otherwise                   = Nothing
-
+-- This function attempts to find factorisation. This loops through all the
+-- vector elements in the kernel of the matrix until a factorisation is found.
+-- The number @generator@ determines which combination of basis elements is
+-- used when cooking up a solution. It does so via its representation base 2.
+-- This solution is chosen and the corresponding @factor@ is computed.
 findFactor :: Integer -> Integer -> V.Vector S.IntSet -> V.Vector (S.IntSet, SP.PrimeIntSet) -> Integer
 findFactor n generator solutionRules indexedFactorisations
     | solution == S.empty            = error "Parametres are not large enough."
@@ -159,8 +132,51 @@ findFactor n generator solutionRules indexedFactorisations
             factor = gcd (x - y) n
             x = findFirstSquare solution n
             y = findSecondSquare solution indexedFactorisations n
-            -- Only attempts to find solutions by selecting the columns
-            -- Does not check linear combinations of them
-            -- Is this even necessary?
             solution = convertToSolution parametres solutionRules
             parametres = generateParametres solutionRules generator
+
+-- Given a solution, the value of x^2 - n is recomputed. Note that, by contruction,
+-- the solution IntSet consists of values which correspond to columns in the
+-- original sieving interval (before sieving). Thus, in order to compute x^2 - n,
+-- it is sufficient to add the square root.
+findFirstSquare :: S.IntSet -> Integer -> Integer
+findFirstSquare solution n = S.fold construct 1 solution
+    where
+        construct current previous = ((intToInteger current + squareRoot) * previous) `mod` n
+        squareRoot = integerSquareRoot n + 1
+
+-- Finds the factorisations corresponding to the selected solutions and computes
+-- the total number of times a given prime occurs in the selected factorisations.
+-- By construction, for any given prime, this number is even. From here, a
+-- square root is computed.
+findSecondSquare :: S.IntSet -> V.Vector (S.IntSet, SP.PrimeIntSet) -> Integer -> Integer
+findSecondSquare solution indexedFactorisations n = I.foldrWithKey computeRoot 1 countSquares
+    where
+        computeRoot key power previous = (intToInteger key ^ (power `div` 2 :: Int) * previous) `mod` n
+        countSquares = V.foldl countPowers I.empty squares
+        countPowers = SP.foldr (\key im -> I.insertWith (+) (unPrime key) 1 im)
+        squares = V.mapMaybe selectSolution indexedFactorisations
+        selectSolution (index, factorisation)
+            | index `S.isSubsetOf` solution = Just factorisation
+            | otherwise                     = Nothing
+
+-- Given the IntMap computed in generateParametres, a solution is constructed
+-- summing through all those columns whose value in IntMap is False.
+convertToSolution :: I.IntMap Bool -> V.Vector S.IntSet -> S.IntSet
+convertToSolution parametres = V.ifoldr add S.empty
+    where
+        add index rule previous = case I.lookup index parametres of
+            Just True  -> previous
+            Just False -> xor rule previous
+            Nothing    -> previous
+            where
+                xor a b = (a S.\\ b) <> (b S.\\ a)
+
+-- This function converts a seed @generator@ into a binary vector which
+-- determines which basis elements are summed. The key in IntMap is the
+-- number of the column (before sieving)
+generateParametres :: V.Vector S.IntSet -> Integer -> I.IntMap Bool
+generateParametres _ 0 = I.empty
+generateParametres solutionRules generator = case V.findIndex (/= S.empty) solutionRules of
+    Just column -> I.insert column (even generator) (generateParametres (solutionRules V.// [(column, S.empty)]) (generator `div` 2))
+    Nothing     -> I.empty
--- test-suite/Math/NumberTheory/Primes/QuadraticSieveTests.hs
+++ test-suite/Math/NumberTheory/Primes/QuadraticSieveTests.hs
@@ -2,39 +2,39 @@ module Math.NumberTheory.Primes.QuadraticSieveTests
   ( testSuite
   ) where
 
+import qualified Data.Vector as V
+import qualified Data.IntSet as S
+import qualified Math.NumberTheory.Primes.IntSet as SP
+import Control.Monad.ST
 import Test.Tasty
 import Math.NumberTheory.TestUtils
 import Math.NumberTheory.Primes
 import Math.NumberTheory.Primes.Factorisation.QuadraticSieve
-import Control.Monad.ST
-import qualified Data.IntSet as S
-import qualified Math.NumberTheory.Primes.IntSet as SP
-import qualified Data.Vector as V
 
+-- Solves a matrix by finding a basis for its kernel. Checks whether each
+-- basis element is an actual solution for the matrix.
 linearSolver :: [[Prime Int]] -> Bool
 linearSolver listMatrix = runST $ do
     let s = length listMatrix
         matrix = V.fromList (map SP.fromList listMatrix)
-        indexedMatrix = V.zip (V.generate s (S.singleton . id)) matrix
+        indexedMatrix = V.zip (V.generate s S.singleton) matrix
     indexedMatrixM <- V.thaw indexedMatrix
     gaussianEliminationM indexedMatrixM
     indexedMatrixF <- V.unsafeFreeze indexedMatrixM
 
-    let someSolutions = V.mapMaybe select indexedMatrixF
-        select (is, _) = case (S.null is) of
-            True  -> Nothing
-            False -> Just is
+    let someSolutions = V.mapMaybe (\(is,_) -> if S.null is then Nothing else Just is) indexedMatrixF
         listOfSolutions = V.toList someSolutions
 
     pure (checkSolutions matrix listOfSolutions)
 
+-- This checks through all the vector in the basis of the kernel.
 checkSolutions :: V.Vector SP.PrimeIntSet -> [S.IntSet] -> Bool
 checkSolutions _ [] = True
-checkSolutions matrix (x:xs) = isZero && (checkSolutions matrix xs)
+checkSolutions matrix (x:xs) = isZero && checkSolutions matrix xs
     where
-        isZero = (combination == SP.empty)
+        isZero = combination == SP.empty
         combination = S.foldr add SP.empty x
-        add columnIndex acc = xor (matrix V.! columnIndex) acc
+        add columnIndex = xor (matrix V.! columnIndex)
         xor u v = (u SP.\\ SP.unPrimeIntSet v) <> (v SP.\\ SP.unPrimeIntSet u)
 
 testSuite :: TestTree
