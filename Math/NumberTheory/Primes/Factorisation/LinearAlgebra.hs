f92995aa854e2aae3d605566caaa836d5a826b24
folidota <federico.bongiorno14@imperial.ac.uk>
2020-06-18 16:48:03 +0100

    Removing rows

--- Math/NumberTheory/Primes/Factorisation/LinearAlgebra.hs
+++ Math/NumberTheory/Primes/Factorisation/LinearAlgebra.hs
@@ -1,9 +1,13 @@
 module Math.NumberTheory.Primes.Factorisation.LinearAlgebra
-  (
+  ( SBVector(..)
+  , SBMatrix(..)
+  , linearSolve
+  , firstStep
   ) where
 
 import Data.Semigroup ()
-import Data.List
+import System.Random
+import qualified Data.List as L
 import qualified Data.IntSet as S
 import qualified Data.IntMap as I
 
@@ -25,15 +29,21 @@ mult (SBMatrix matrix) (SBVector vector) = foldMap (matrix I.!) (S.toList vector
 
 -- Multiplication of matrices
 instance Semigroup SBMatrix where
-  m1 <> (SBMatrix m2) = SBMatrix (I.map (mult m1) m2)
+  m1 <> (SBMatrix m2) = SBMatrix (I.map (m1 `mult`) m2)
 
 size :: SBMatrix -> Int
 size (SBMatrix m) = I.size m
 
--- -- The input is a matrix B and a random vector z
--- firstStep :: SBMatrix -> SBVector -> [SBMatrix]
--- firstStep matrix z = map (x *) matrixPowers
---   where
---     matrixPowers = L.iterate
---     x = -- Random vector
---     l = 2 * size matrix
+-- The input is a matrix B and a random vector z
+firstStep :: SBMatrix -> SBVector -> [SBVector]
+firstStep m z = map (x `mult`) matrixPowers
+  where
+    matrixPowers = L.iterate (m `mult`) z
+    x = SBMatrix (I.singleton 1 (SBVector S.empty)) -- Random matrix of size 1 * (largest Prime)
+
+linearSolve :: SBMatrix -> Int -> [SBVector]
+linearSolve m t = [z]
+  where
+    seed = mkStdGen 68431698431
+    z = SBVector (S.fromList (L.take 100 (randomRs (0,t) seed)))
+    -- firstStep m z
