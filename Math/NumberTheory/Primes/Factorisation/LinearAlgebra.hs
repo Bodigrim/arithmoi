a778369c3c2994c9523e3ac664d3d65a70b2d528
folidota <federico.bongiorno14@imperial.ac.uk>
2020-07-12 00:18:22 +0100

    New implementation of mult

--- Math/NumberTheory/Primes/Factorisation/LinearAlgebra.hs
+++ Math/NumberTheory/Primes/Factorisation/LinearAlgebra.hs
@@ -11,6 +11,9 @@ import qualified Data.List as L
 import qualified Data.IntSet as S
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
+import qualified Data.Vector.Unboxed.Mutable as MU
+import Control.Monad.ST
+import Data.Maybe
 import Debug.Trace
 import Data.Semigroup()
 import System.Random
@@ -18,10 +21,12 @@ import System.IO.Unsafe
 import GHC.Clock
 import Data.Bit
 import Data.Bits
+import Data.Semigroup
+import Data.Foldable
 
 -- Sparse Binary Vector
 newtype SBVector = SBVector
-  { set :: S.IntSet
+  { unSBVector :: S.IntSet
   } deriving (Show, Eq)
 
 -- Vectors form a group (in particular a monoid) under addition.
@@ -33,11 +38,19 @@ instance Monoid SBVector where
 
 dot :: SBVector -> SBVector -> Bit
 dot (SBVector v1) (SBVector v2) = Bit (even (S.size (v1 `S.intersection` v2)))
+
 -- Sparse Binary Matrix
 newtype SBMatrix = SBMatrix (V.Vector SBVector) deriving (Show)
 
 mult :: SBMatrix -> SBVector -> SBVector
-mult (SBMatrix matrix) (SBVector vector) = foldMap (matrix V.!) (S.toList vector)
+mult m@(SBMatrix matrix) (SBVector vector) = runST $ do
+  -- it would be better to cache maximal length somewhere
+  -- let len = getMax (fromJust (foldMap (fmap (Max . fst) . S.maxView . unSBVector) matrix)) + 1
+  vs <- MU.new $ size m
+  traverse_ (traverse_ (unsafeFlipBit vs) . S.toList . unSBVector . (matrix V.!)) (S.toList vector)
+  ws <- U.unsafeFreeze vs
+  pure $ SBVector $ S.fromDistinctAscList $ listBits ws
+-- mult (SBMatrix matrix) (SBVector vector) = foldMap (matrix V.!) (S.toList vector)
 
 size :: SBMatrix -> Int
 size (SBMatrix m) = V.length m
@@ -45,16 +58,16 @@ size (SBMatrix m) = V.length m
 linearSolve :: SBMatrix -> SBVector
 linearSolve matrix = linearSolveHelper 1 matrix randomVectors 0 0
   where
-    -- Make sure random vectors are not empty. 0.05 is the sparsity of random vectors
-    randomVectors = getRandomVectors [0..(size matrix - 1)] 0.5 (mkStdGen (fromIntegral (unsafePerformIO getMonotonicTimeNSec)))
+    -- Make sure random vectors are not empty. The floating point number is the sparsity of the random vectors
+    randomVectors = getRandomVectors [0..(size matrix - 1)] 0.1 $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
 
 linearSolveHelper :: F2Poly -> SBMatrix -> [SBVector] -> Int -> Int -> SBVector
 linearSolveHelper previousPoly matrix (z : x : otherVecs) counter totalCounter
-  | totalCounter > 99                           = error "Fail"
-  | potentialSolution == mempty && counter <= 9 = linearSolveHelper potentialMinPoly matrix (z : otherVecs) (counter + 1) (totalCounter + 1)
-  | potentialSolution == mempty && counter > 9  = linearSolveHelper 1 matrix otherVecs 0 (totalCounter + 1)
+  | potentialSolution == mempty && totalCounter > 99 = error "Fail"
+  | potentialSolution == mempty && counter <= 9      = trace ("Counter: " ++ show (counter + 1) ++ "\nTotal Counter: " ++ show (totalCounter + 1)) $ linearSolveHelper potentialMinPoly matrix (z : otherVecs) (counter + 1) (totalCounter + 1)
+  | potentialSolution == mempty && counter > 9       = trace ("Counter: " ++ show (counter + 1) ++ "\nTotal Counter: " ++ show (totalCounter + 1)) $ linearSolveHelper 1 matrix otherVecs 0 (totalCounter + 1)
   -- This is a good solution.
-  | otherwise                   = trace ("Counter: " ++ show counter ++ "\nTotal Counter: " ++ show totalCounter) potentialSolution
+  | otherwise                                        = trace ("Counter: " ++ show (counter + 1) ++ "\nTotal Counter: " ++ show (totalCounter + 1)) $ potentialSolution
   where
     potentialSolution = findSolution singularities matrix almostZeroVector
     almostZeroVector = evaluate matrix z reducedMinPoly
@@ -102,9 +115,6 @@ generateData :: SBMatrix -> SBVector -> SBVector -> F2Poly
 generateData matrix z x = toF2Poly $ U.fromList $ reverse $ map (x `dot`) matrixPowers
   where
     matrixPowers = L.take (2 * size matrix) $ L.iterate (matrix `mult`) z
-    -- Make sure x is not empty.
-    -- Check matrix indices
-    -- trace ("Size of x: " ++ (show ((I.foldr (\entry acc -> acc + (if entry == mempty then 0 else 1)) 0 ix) :: Int))) $
 
 -- Infinite lists of random vectors
 getRandomVectors :: [Int] -> Double -> StdGen -> [SBVector]
@@ -118,8 +128,7 @@ getRandomVectors rows sparsity gen = go randomEntries
         listOfEntries = fmap fst $ filter (\(_, rDouble) -> rDouble < sparsity) frontOfList
         (frontOfList, backOfList) = L.splitAt (length rows) list
 
--- Informal way of testing large matrices
--- Input number of columns of matrix and sparsity coefficient
+-- Input number of columns of matrix and sparsity coefficient. It returns a random matrix.
 testLinearSolver :: Int -> Double -> Bool
 testLinearSolver dim sparsity = mat `mult` sol == mempty
   where
@@ -128,7 +137,6 @@ testLinearSolver dim sparsity = mat `mult` sol == mempty
     -- -2 is arbitrary. It means that the number of rows is at most one less than
     -- the number of columns
     listOfColumns = L.take dim $ getRandomVectors [0..(dim - 2)] sparsity $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
-
 
 -- 1) Investigate rare cases where code does not terminates.
 -- 2) Implement dense matrices rather than sparse.
