c9618524ad8f8d4fd8b9ae736ce2a402b2019b71
folidota <federico.bongiorno14@imperial.ac.uk>
2020-07-14 22:33:49 +0100

    Dense binary vectors

--- Math/NumberTheory/Primes/Factorisation/LinearAlgebra.hs
+++ Math/NumberTheory/Primes/Factorisation/LinearAlgebra.hs
@@ -1,19 +1,23 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Math.NumberTheory.Primes.Factorisation.LinearAlgebra
   ( SBVector(..)
+  , DBVector(..)
   , SBMatrix(..)
+  , isNull
+  , dot
   , mult
   , size
   , linearSolve
   , testLinearSolver
+  , getRandomSBVectors
   ) where
 
 import qualified Data.List as L
-import qualified Data.IntSet as S
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as U
 import qualified Data.Vector.Unboxed.Mutable as MU
 import Control.Monad.ST
-import Data.Maybe
 import Debug.Trace
 import Data.Semigroup()
 import System.Random
@@ -21,68 +25,73 @@ import System.IO.Unsafe
 import GHC.Clock
 import Data.Bit
 import Data.Bits
-import Data.Semigroup
 import Data.Foldable
 
 -- Sparse Binary Vector
 newtype SBVector = SBVector
-  { unSBVector :: S.IntSet
+  { unSBVector :: U.Vector Int
+  } deriving (Show)
+
+-- Dense Binary Vector
+newtype DBVector = DBVector
+  { unDBVector :: U.Vector Bit
   } deriving (Show, Eq)
 
--- Vectors form a group (in particular a monoid) under addition.
-instance Semigroup SBVector where
-  SBVector v1 <> SBVector v2 = SBVector ((v1 S.\\ v2) <> (v2 S.\\ v1))
-
-instance Monoid SBVector where
-  mempty = SBVector mempty
-
-dot :: SBVector -> SBVector -> Bit
-dot (SBVector v1) (SBVector v2) = Bit (even (S.size (v1 `S.intersection` v2)))
-
 -- Sparse Binary Matrix
-newtype SBMatrix = SBMatrix (V.Vector SBVector) deriving (Show)
+newtype SBMatrix = SBMatrix
+  { unSBMatrix :: V.Vector SBVector
+  } deriving (Show)
 
--- -- replacing IntSet by U.Vector Int
--- newtype SBMatrix = SBMatrix (V.Vector (U.Vector Int))
+instance Semigroup DBVector where
+  DBVector v1 <> DBVector v2 = DBVector $ zipBits xor v1 v2
 
--- Use
--- U.Vector Int for sparse binary vectors
--- U.Vector Bit for dense  binary vectors
+isNull :: DBVector -> Bool
+isNull (DBVector v) = null $ listBits v
 
-mult :: SBMatrix -> SBVector -> SBVector
-mult m@(SBMatrix matrix) (SBVector vector) = runST $ do
-  -- it would be better to cache maximal length somewhere
-  let len = getMax (fromJust (foldMap (fmap (Max . fst) . S.maxView . unSBVector) matrix)) + 1
-  -- vs <- MU.new (size m)
-  -- traceShowM (size m, len)
-  vs <- MU.new len
-  traverse_ (traverse_ (flipBit vs) . S.toList . unSBVector . (matrix V.!)) (S.toList vector)
+dot :: DBVector -> DBVector -> Bit
+dot (DBVector v1) (DBVector v2) = Bit $ odd . countBits $ zipBits (.&.) v1 v2
+
+-- mult :: SBMatrix -> DBVector -> DBVector
+-- mult (SBMatrix matrix) (DBVector vector) = runST $ do
+--   -- let len = getMax (fromJust (foldMap (fmap (Max . fst) . S.maxView . unSBVector) matrix)) + 1
+--   -- vs <- MU.new (size m)
+--   -- traceShowM (size m, len)
+--   vs <- MU.new (U.length vector)
+--   traverse_ (traverse_ (unsafeFlipBit vs) . S.toList . unSBVector . (matrix V.!)) (S.toList vector)
+--   ws <- U.unsafeFreeze vs
+--   pure $ DBVector ws
+
+mult :: SBMatrix -> DBVector -> DBVector
+mult (SBMatrix matrix) (DBVector vector) = runST $ do
+  vs <- MU.new (U.length vector)
+  traverse_ (traverse_ (unsafeFlipBit vs) . U.toList . unSBVector . (matrix V.!)) (listBits vector)
   ws <- U.unsafeFreeze vs
-  pure $ SBVector $ S.fromDistinctAscList $ listBits ws
--- mult (SBMatrix matrix) (SBVector vector) = foldMap (matrix V.!) (S.toList vector)
+  pure $ DBVector ws
 
--- traverse_ = flip forM_ (S.toList vector) $ \column ->
---
--- forM_ (S.toList vector) $ \columnIndex ->
---   forM_ (S.toList $ unSBVector (matrix V.! columnIndex)) $ \i ->
---     unsafeFlipBit vs i
+-- -- traverse_ = flip forM_ (S.toList vector) $ \column ->
+-- --
+-- -- forM_ (S.toList vector) $ \columnIndex ->
+-- --   forM_ (S.toList $ unSBVector (matrix V.! columnIndex)) $ \i ->
+-- --     unsafeFlipBit vs i
+
+-- traverse_ (traverse_ (unsafeFlipBit vs) . S.toList . unSBVector . (matrix V.!)) (S.toList vector)
 
 size :: SBMatrix -> Int
 size (SBMatrix m) = V.length m
 
-linearSolve :: SBMatrix -> SBVector
-linearSolve matrix = linearSolveHelper 1 matrix randomVectors 0 0
+linearSolve :: SBMatrix -> DBVector
+linearSolve matrix = linearSolveHelper 1 matrix randomVectors 1 1
   where
     -- Make sure random vectors are not empty. The floating point number is the sparsity of the random vectors
-    randomVectors = getRandomVectors [0..(size matrix - 1)] 0.1 $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
+    randomVectors = getRandomDBVectors (size matrix) 0.5 $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
 
-linearSolveHelper :: F2Poly -> SBMatrix -> [SBVector] -> Int -> Int -> SBVector
+linearSolveHelper :: F2Poly -> SBMatrix -> [DBVector] -> Int -> Int -> DBVector
 linearSolveHelper previousPoly matrix (z : x : otherVecs) counter totalCounter
-  | potentialSolution == mempty && totalCounter > 99 = error "Fail"
-  | potentialSolution == mempty && counter <= 9      = trace ("Counter: " ++ show (counter + 1) ++ "\nTotal Counter: " ++ show (totalCounter + 1)) $ linearSolveHelper potentialMinPoly matrix (z : otherVecs) (counter + 1) (totalCounter + 1)
-  | potentialSolution == mempty && counter > 9       = trace ("Counter: " ++ show (counter + 1) ++ "\nTotal Counter: " ++ show (totalCounter + 1)) $ linearSolveHelper 1 matrix otherVecs 0 (totalCounter + 1)
+  | isNull potentialSolution && totalCounter > 100 = trace ("Fail: " ++ show matrix) $ error "Incorrect algorithm"
+  | isNull potentialSolution && counter <= 9       = trace ("Counter: " ++ show counter ++ "\nTotal Counter: " ++ show totalCounter) $ linearSolveHelper potentialMinPoly matrix (z : otherVecs) (counter + 1) (totalCounter + 1)
+  | isNull potentialSolution && counter > 9        = trace ("Counter: " ++ show counter ++ "\nTotal Counter: " ++ show totalCounter) $ linearSolveHelper 1 matrix otherVecs 1 (totalCounter + 1)
   -- This is a good solution.
-  | otherwise                                        = trace ("Counter: " ++ show (counter + 1) ++ "\nTotal Counter: " ++ show (totalCounter + 1)) $ potentialSolution
+  | otherwise                                      = trace ("Counter: " ++ show counter ++ "\nTotal Counter: " ++ show totalCounter) $ potentialSolution
   where
     potentialSolution = findSolution singularities matrix almostZeroVector
     almostZeroVector = evaluate matrix z reducedMinPoly
@@ -91,11 +100,14 @@ linearSolveHelper previousPoly matrix (z : x : otherVecs) counter totalCounter
     potentialMinPoly = lcm previousPoly candidateMinPoly
     candidateMinPoly = findCandidatePoly matrix z x
 
-findSolution :: [Bit] -> SBMatrix -> SBVector -> SBVector
-findSolution [] _ _ = mempty
+findSolution :: [Bit] -> SBMatrix -> DBVector -> DBVector
+-- It is ideal to define a family of parameters indexed by the length of the vector.
+-- This is then an abelian group. In this workaround, @DBVector (U.empty)@ can
+-- be used as it is not summed to any other element.
+findSolution [] _ _ = DBVector (U.empty)
 findSolution (_ : xs) matrix vector
-  | result == mempty = vector
-  | otherwise        = findSolution xs matrix result
+  | isNull result = vector
+  | otherwise     = findSolution xs matrix result
   where
     result = matrix `mult` vector
 
@@ -103,10 +115,13 @@ findSolution (_ : xs) matrix vector
 -- returns p(A)w. It assumes the first coefficient of p is non zero,
 -- in particular that it is non empty. This makes the implementation
 -- easier as there is no need to write the identity matrix.
-evaluate :: SBMatrix -> SBVector -> [Bit] -> SBVector
-evaluate matrix w = foldr (\coeff acc -> (matrix `mult` acc) <> (if unBit coeff then w else mempty)) mempty
+evaluate :: SBMatrix -> DBVector -> [Bit] -> DBVector
+-- Here @DBVector (U.empty)@ cannot be used since summing it with any other
+-- vector gives the empty vector again.
+evaluate matrix w = foldr (\coeff acc -> (matrix `mult` acc) <> (if unBit coeff then w else zero)) zero
+  where zero = DBVector $ U.replicate (size matrix) (Bit False)
 
-findCandidatePoly :: SBMatrix -> SBVector -> SBVector -> F2Poly
+findCandidatePoly :: SBMatrix -> DBVector -> DBVector -> F2Poly
 findCandidatePoly matrix z x = berlekampMassey dim errorPoly randomSequence
   where
     randomSequence = generateData matrix z x
@@ -126,32 +141,42 @@ berlekampMassey dim = go 1 0
           (q, r) = quotRem a b
 
 -- The input is a matrix B and a random vector z
-generateData :: SBMatrix -> SBVector -> SBVector -> F2Poly
+generateData :: SBMatrix -> DBVector -> DBVector -> F2Poly
 generateData matrix z x = toF2Poly $ U.fromList $ reverse $ map (x `dot`) matrixPowers
   where
     matrixPowers = L.take (2 * size matrix) $ L.iterate (matrix `mult`) z
 
--- Infinite lists of random vectors
-getRandomVectors :: [Int] -> Double -> StdGen -> [SBVector]
-getRandomVectors rows sparsity gen = go randomEntries
+-- Infinite lists of random DBVectors.
+getRandomDBVectors :: Int -> Double -> StdGen -> [DBVector]
+getRandomDBVectors numberOfColumns density gen = go $ randomRs (0, 1) gen
   where
-    randomEntries = zip (cycle rows) (randomRs (0, 1) gen)
-    go :: [(Int, Double)] -> [SBVector]
+    go :: [Double] -> [DBVector]
     go list = newVector : go backOfList
       where
-        newVector = SBVector (S.fromList listOfEntries)
-        listOfEntries = fmap fst $ filter (\(_, rDouble) -> rDouble < sparsity) frontOfList
-        (frontOfList, backOfList) = L.splitAt (length rows) list
+        newVector = DBVector $ U.fromList $ map (\d -> Bit (d > density)) frontOfList
+        (frontOfList, backOfList) = L.splitAt numberOfColumns list
 
 -- Input number of columns of matrix and sparsity coefficient. It returns a random matrix.
 testLinearSolver :: Int -> Double -> Bool
-testLinearSolver dim sparsity = mat `mult` sol == mempty
+testLinearSolver dim density = isNull $ mat `mult` sol
   where
     sol = linearSolve mat
     mat = SBMatrix (V.fromList listOfColumns)
     -- -2 is arbitrary. It means that the number of rows is at most one less than
     -- the number of columns
-    listOfColumns = L.take dim $ getRandomVectors [0..(dim - 2)] sparsity $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
+    listOfColumns = L.take dim $ getRandomSBVectors (dim - 1) density $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
+
+-- Infinite lists of random SBVectors. Only used for testing.
+getRandomSBVectors :: Int -> Double -> StdGen -> [SBVector]
+getRandomSBVectors numberOfRows density gen = go randomEntries
+  where
+    randomEntries = zip (cycle [0..(numberOfRows - 1)]) (randomRs (0, 1) gen)
+    go :: [(Int, Double)] -> [SBVector]
+    go list = newVector : go backOfList
+      where
+        newVector = SBVector (U.fromList listOfEntries)
+        listOfEntries = fmap fst $ filter (\(_, rDouble) -> rDouble < density) frontOfList
+        (frontOfList, backOfList) = L.splitAt numberOfRows list
 
 -- 1) Investigate rare cases where code does not terminates.
 -- 2) Implement dense matrices rather than sparse.
--- test-suite/Math/NumberTheory/Primes/LinearAlgebraTests.hs
+++ test-suite/Math/NumberTheory/Primes/LinearAlgebraTests.hs
@@ -6,31 +6,19 @@ import Test.Tasty
 import Math.NumberTheory.TestUtils
 import Math.NumberTheory.Primes.Factorisation.LinearAlgebra
 import qualified Data.List as L
-import qualified Data.IntSet as S
 import qualified Data.Vector as V
 import System.Random
 import System.IO.Unsafe
 import GHC.Clock
 
 testLinear :: Int -> Bool
-testLinear dim = dim < 2 || (mat `mult` sol == mempty)
+testLinear dim = dim < 2 || isNull (mat `mult` sol)
   where
     sol = linearSolve mat
     mat = SBMatrix (V.fromList listOfColumns)
-    -- -2 is arbitrary. It means that the number of rows is at most one less than
-    -- the number of columns
-    listOfColumns = L.take dim $ getRandomColumns [0..(dim - 2)] 0.3 $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
-
-getRandomColumns :: [Int] -> Double -> StdGen -> [SBVector]
-getRandomColumns rows sparsity gen = go randomEntries
-  where
-    randomEntries = zip (cycle rows) (randomRs (0, 1) gen)
-    go :: [(Int, Double)] -> [SBVector]
-    go list = newVector : go backOfList
-      where
-        newVector = SBVector (S.fromList listOfEntries)
-        listOfEntries = fmap fst $ filter (\(_, rDouble) -> rDouble < sparsity) frontOfList
-        (frontOfList, backOfList) = L.splitAt (length rows) list
+    -- -1 is arbitrary. It means that the number of rows is at most one less than
+    -- the number of columns. 0.4 is the density
+    listOfColumns = L.take dim $ getRandomSBVectors (dim - 1) 0.4 $ mkStdGen $ fromIntegral $ unsafePerformIO getMonotonicTimeNSec
 
 testSuite :: TestTree
 testSuite = testGroup "QuadraticSieve"
