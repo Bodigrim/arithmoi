19a2e0e9a1e4aed9b63f9657c0094f527fa71dde
folidota <federico.bongiorno14@imperial.ac.uk>
2020-07-24 01:11:31 +0100

    Modified test-suite

--- test-suite/Math/NumberTheory/Primes/QuadraticSieveTests.hs
+++ test-suite/Math/NumberTheory/Primes/QuadraticSieveTests.hs
@@ -13,7 +13,7 @@ import Math.NumberTheory.Primes.Factorisation.QuadraticSieve
 quadraticRelation :: (Prime (Large Int), Prime (Large Int)) -> Bool
 quadraticRelation (unPrime -> Large p, unPrime -> Large q)
   | p == q    = True
-  | otherwise = all checkQuadratic (findSquares (toInteger p * toInteger q) 1000 2000)
+  | otherwise = checkQuadratic (findSquares (toInteger p * toInteger q) 1000 2000)
     where
       checkQuadratic (x, y) = (x * x - y * y) `mod` (toInteger p * toInteger q) == 0
 
