15ab6f2769b94c915ae4dc09b192a11b306c8bab
folidota <federico.bongiorno14@imperial.ac.uk>
2020-08-21 16:58:40 +0100

    Changed quadraticSieve.app

--- app/QuadraticSieve.hs
+++ app/QuadraticSieve.hs
@@ -9,6 +9,3 @@ main = print $ quadraticSieveManual n qsc
     m = 3 * t `div` 2 + 900
     t = floor ((*12) . sqrt . exp . sqrt $ log (fromInteger n) * log (log (fromInteger n)) :: Double)
     n = 313171213163547737474104867123
-     -- 313171213163547737474104867123
-     -- 820239374043355485537790987979115484333
-     -- 1000000000100000000000000000074000000005700000000000000000969
