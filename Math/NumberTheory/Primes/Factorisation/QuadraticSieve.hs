dc9d2859f2fd398749b30197201d40924f9d2c30
folidota <federico.bongiorno14@imperial.ac.uk>
2020-08-21 13:24:28 +0100

    Changed executable and corrected a parameter

--- Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
+++ Math/NumberTheory/Primes/Factorisation/QuadraticSieve.hs
@@ -61,13 +61,13 @@ data QuadraticSieveConfig = QuadraticSieveConfig
 autoConfig :: Integer -> QuadraticSieveConfig
 autoConfig n = QuadraticSieveConfig t m k h
   where
-    h = intLog2 t + 4
+    h = intLog2 t + 6
     k = max 0 (l `div` 10)
-    m = 3 * t `div` 2
+    m = t
     t
       | l < 4    = integerToInt n `div` 2
       | l < 8    = integerToInt $ integerSquareRoot n
-      | otherwise = max (42 - l) 1 * floor (exp (sqrt (le * log le) / 2) :: Double)
+      | otherwise = max (41 - l) 1 * floor (exp (sqrt (le * log le) / 2) :: Double)
     -- number of digits of n
     l = integerLog10 n
     le = fromIntegral l * log 10
@@ -139,7 +139,7 @@ findSquares n (QuadraticSieveConfig t m k h) = runST $ do
             smoothNumbers = previousSmoothNumbers `M.union` newSmoothNumbers
             matrixSmoothNumbers
               | trace ("Matrix dimension: " ++ show (numberOfConstraints, length mat)) False = undefined
-              | numberOfConstraints < length mat = take (numberOfConstraints + 4 * (k + 1)) $ M.assocs smoothNumbers
+              | numberOfConstraints < length mat = take (numberOfConstraints + 3 * (k + 2)) $ M.assocs smoothNumbers
               | otherwise                        = goSelfInitSieving smoothNumbers otherCoeffs
               where
                 numberOfConstraints = S.size $ foldMap convertToSet mat
@@ -152,7 +152,7 @@ findSquares n (QuadraticSieveConfig t m k h) = runST $ do
     goSolving :: Int -> Int -> [(Integer, Integer)]
     goSolving seed counter
       | counter < 5 = firstSquare `seq` secondSquare `seq` (firstSquare, secondSquare) : goSolving (seed + 1) (counter + 1)
-      | otherwise   = findSquares n $ QuadraticSieveConfig t (m + 100 * (k + 1) * (k + 1)) k h
+      | otherwise   = findSquares n $ QuadraticSieveConfig (t + 50 * (k + 1)) (m + 50 * (k + 1) * (k + 1)) k h
       where
         firstSquare = findFirstSquare n (fmap fst squaresData)
         secondSquare = findSecondSquare n (fmap snd squaresData)
--- app/QuadraticSieve.hs
+++ app/QuadraticSieve.hs
@@ -1,13 +1,15 @@
 import Math.NumberTheory.Primes.Factorisation.QuadraticSieve
-
+import Math.NumberTheory.Logarithms
 main :: IO ()
 main = print $ quadraticSieveManual n qsc
   where
     qsc = QuadraticSieveConfig t m k h
-    h = 18
+    h = 19
     k = 2
-    m = 3 * t `div` 2 + 900
-    t = floor ((*12) . sqrt . exp . sqrt $ log (fromInteger n) * log (log (fromInteger n)) :: Double)
+    m = t + 900
+    t = 12 * floor (exp (sqrt (le * log le) / 2) :: Double)
+    l = integerLog10 n
+    le = fromIntegral l * log 10
     n = 313171213163547737474104867123
      -- 313171213163547737474104867123
      -- 820239374043355485537790987979115484333
