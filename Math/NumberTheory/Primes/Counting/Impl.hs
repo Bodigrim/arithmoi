Bodigrim/arithmoi

commit f50708c3cac986aa750e03c0ee70a4c5108cee92

Move countToNth and countAll under M.NT.Primes.Counting

Math/NumberTheory/Primes/Counting/Impl.hs
@@ -21,17 +21,19 @@ module Math.NumberTheory.Primes.Counting.Impl
 #include "MachDeps.h"
 
 import Math.NumberTheory.Primes.Sieve.Eratosthenes
-    (PrimeSieve(..), primeList, primeSieve, psieveFrom, sieveTo, sieveBits, sieveRange, countFromTo, countToNth, countAll)
+    (PrimeSieve(..), primeList, primeSieve, psieveFrom, sieveTo, sieveBits, sieveRange, countFromTo)
 import Math.NumberTheory.Primes.Sieve.Indexing (toPrim, idxPr)
 import Math.NumberTheory.Primes.Counting.Approximate (nthPrimeApprox, approxPrimeCount)
 import Math.NumberTheory.Primes.Types
 import Math.NumberTheory.Roots
 import Math.NumberTheory.Unsafe
+import Math.NumberTheory.Utils
 
-import Data.Array.ST
 import Control.Monad.ST
+import Data.Array.ST
 import Data.Bits
 import Data.Int
+import Unsafe.Coerce
 
 -- | Maximal allowed argument of 'primeCount'. Currently 8e18.
 primeCountMaxArg :: Integer
@@ -409,3 +411,66 @@ cpGpAr = runSTUArray $ do
     note 26 13
     accumulate 2 30027
 
+-------------------------------------------------------------------------------
+-- Prime counting
+
+#if SIZEOF_HSWORD == 8
+
+#define WSHFT 6
+#define TOPB 32
+#define TOPM 0xFFFFFFFF
+
+#else
+#define WSHFT 5
+#define TOPB 16
+#define TOPM 0xFFFF
+
+#endif
+
+-- find the n-th set bit in a list of PrimeSieves,
+-- aka find the (n+3)-rd prime
+countToNth :: Int -> [PrimeSieve] -> Integer
+countToNth !_ [] = error "countToNth: Prime stream ended prematurely"
+countToNth !n (PS v0 bs : more) = go n 0
+  where
+    wa :: UArray Int Word
+    wa = unsafeCoerce bs
+
+    go !k i
+      | i == snd (bounds wa)
+      = countToNth k more
+      | otherwise
+      = let w = unsafeAt wa i
+            bc = popCount w
+        in if bc < k
+          then go (k-bc) (i+1)
+          else let j = bc - k
+                   px = top w j bc
+               in v0 + toPrim (px + (i `shiftL` WSHFT))
+
+-- count all set bits in a chunk, do it wordwise for speed.
+countAll :: PrimeSieve -> Int
+countAll (PS _ bs) = go 0 0
+  where
+    wa :: UArray Int Word
+    wa = unsafeCoerce bs
+
+    go !ct i
+      | i == snd (bounds wa)
+      = ct
+      | otherwise
+      = go (ct + popCount (unsafeAt wa i)) (i+1)
+
+-- Find the j-th highest of bc set bits in the Word w.
+top :: Word -> Int -> Int -> Int
+top w j bc = go 0 TOPB TOPM bn w
+    where
+      !bn = bc-j
+      go !_ _ !_ !_ 0 = error "Too few bits set"
+      go bs 0 _ _ wd = if wd .&. 1 == 0 then error "Too few bits, shift 0" else bs
+      go bs a msk ix wd =
+        case popCount (wd .&. msk) of
+          lc | lc < ix  -> go (bs+a) a msk (ix-lc) (wd `uncheckedShiftR` a)
+             | otherwise ->
+               let !na = a `shiftR` 1
+               in go bs na (msk `uncheckedShiftR` na) ix wd

Math/NumberTheory/Primes/Sieve/Eratosthenes.hs
@@ -21,8 +21,6 @@ module Math.NumberTheory.Primes.Sieve.Eratosthenes
     , primeList
     , primeSieve
     , countFromTo
-    , countAll
-    , countToNth
     , sieveBits
     , sieveRange
     , sieveTo
@@ -38,13 +36,11 @@ import Data.Bits
 import Data.Coerce
 import Data.Proxy
 import Data.Word
-import Unsafe.Coerce
 
 import Math.NumberTheory.Primes.Sieve.Indexing
 import Math.NumberTheory.Primes.Types
 import Math.NumberTheory.Roots
 import Math.NumberTheory.Unsafe
-import Math.NumberTheory.Utils
 
 #define IX_MASK     0xFFFFF
 #define IX_BITS     20
@@ -71,9 +67,6 @@ lastIndex = sieveBits - 1
 -- Range of a chunk.
 sieveRange :: Int
 sieveRange = 30*sieveBytes
-
-sieveWords :: Int
-sieveWords = sieveBytes `quot` SIZEOF_HSWORD
 
 type CacheWord = Word64
 
@@ -424,52 +417,6 @@ psieveFrom n = makeSieves plim sqlim bitOff valOff cache
                     else fill j (indx+1)
           fill 0 0
 
--- prime counting
-
--- find the n-th set bit in a list of PrimeSieves,
--- aka find the (n+3)-rd prime
-countToNth :: Int -> [PrimeSieve] -> Integer
-countToNth !_ [] = error "countToNth: Prime stream ended prematurely"
-countToNth !n (PS v0 bs : more) = go n 0
-  where
-    wa :: UArray Int Word
-    wa = unsafeCoerce bs
-
-    go !k i
-      | i == sieveWords = countToNth k more
-      | otherwise
-      = let w = unsafeAt wa i
-            bc = popCount w
-        in if bc < k
-          then go (k-bc) (i+1)
-          else let j = bc - k
-                   px = top w j bc
-               in v0 + toPrim (px + (i `shiftL` WSHFT))
-
--- count all set bits in a chunk, do it wordwise for speed.
-countAll :: PrimeSieve -> Int
-countAll (PS _ bs) = go 0 0
-  where
-    wa :: UArray Int Word
-    wa = unsafeCoerce bs
-
-    go !ct i
-      | i == sieveWords = ct
-      | otherwise = go (ct + popCount (unsafeAt wa i)) (i+1)
-
--- Find the j-th highest of bc set bits in the Word w.
-top :: Word -> Int -> Int -> Int
-top w j bc = go 0 TOPB TOPM bn w
-    where
-      !bn = bc-j
-      go !_ _ !_ !_ 0 = error "Too few bits set"
-      go bs 0 _ _ wd = if wd .&. 1 == 0 then error "Too few bits, shift 0" else bs
-      go bs a msk ix wd =
-        case popCount (wd .&. msk) of
-          lc | lc < ix  -> go (bs+a) a msk (ix-lc) (wd `uncheckedShiftR` a)
-             | otherwise ->
-               let !na = a `shiftR` 1
-               in go bs na (msk `uncheckedShiftR` na) ix wd
 
 {-# INLINE delta #-}
 delta :: Int -> Int

commit 9a7cdeb85a30054ef2ddf5d946bab0d93cb4a767

Fix primeCount on arguments > 5e14
It was broken in f50708c3cac986aa750e03c0ee70a4c5108cee92

Math/NumberTheory/Primes/Counting/Impl.hs
@@ -431,10 +431,10 @@ countToNth !_ [] = error "countToNth: Prime stream ended prematurely"
 countToNth !n (PS v0 bs : more) = go n 0
   where
     wa :: UArray Int Word
-    wa = unsafeCoerce bs
+    wa = coerceArray bs
 
     go !k i
-      | i == snd (bounds wa)
+      | i > snd (bounds wa)
       = countToNth k more
       | otherwise
       = let w = unsafeAt wa i
@@ -450,13 +450,24 @@ countAll :: PrimeSieve -> Int
 countAll (PS _ bs) = go 0 0
   where
     wa :: UArray Int Word
-    wa = unsafeCoerce bs
+    wa = coerceArray bs
 
     go !ct i
-      | i == snd (bounds wa)
+      | i > snd (bounds wa)
       = ct
       | otherwise
       = go (ct + popCount (unsafeAt wa i)) (i+1)
+
+-- This is dangerous: we rely on the fact that reading a whole word
+-- at the end of ByteArray is permissible, even if its length
+-- is not aligned with word boundaries.
+coerceArray :: UArray Int Bool -> UArray Int Word
+coerceArray (UArray 0 u n ba)
+  | u + 1 == n, n' > 0 = UArray 0 (n' - 1) n' ba
+  where
+    n' = (n + rMASK) `shiftR` wSHFT
+coerceArray (UArray l u n _) =
+    error $ "Cannot coerce array of Bool to array of Word: " ++ show (l, u, n)
 
 -- Find the j-th highest of bc set bits in the Word w.
 top :: Word -> Int -> Int -> Int

test-suite/Math/NumberTheory/Primes/CountingTests.hs
@@ -36,6 +36,8 @@ table =
   , (10^10,  455052511)
   , (10^11,  4118054813)
   , (10^12,  37607912018)
+  -- Enable tests below to validate any changes to 'primeCount' implementation:
+  -- certain routines are not triggered until very large numbers.
   -- , (10^13,  346065536839)
   -- , (10^14,  3204941750802)
   -- , (10^15,  29844570422669)